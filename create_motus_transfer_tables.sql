e612f967b8937089ab56a50d453b6be24989ed37
john brzustowski <jbrzusto@fastmail.fm>
2015-10-19 12:57:28 +0000

    initial attempt at capturing sw version and params

--- create_motus_transfer_tables.sql
+++ create_motus_transfer_tables.sql
@@ -78,7 +78,7 @@ CREATE TABLE hits (
     tsMotus FLOAT(53),                       -- timestamp when this record transferred to motus;
                                              -- NULL means not transferred
     PRIMARY KEY (batchID, ID),
-    FOREIGN KEY (batchID, runID) references batchRunInfo(batchID, runID)
+    FOREIGN KEY (batchID, runID) REFERENCES batchRunInfo(batchID, runID)
 );
 
 
@@ -136,6 +136,43 @@ CREATE TABLE params (
                                              -- means not transferred; unix-style: seconds since 1
                                              -- Jan 1970 GMT
     PRIMARY KEY (batchID, ID)                                                                                                                     
+);
+
+--  TABLE batchSWInfo
+--
+--  maintains information about the versions of software used to generate a batch
+--  of detections.
+--
+
+CREATE TABLE batchSWInfo (
+    batchID INT NOT NULL REFERENCES batches, -- unique identifier of batch for this run
+    progName VARCHAR(16) NOT NULL,           -- identifier of program; e.g. "find_tags", "lotek-plugins.so"
+    progVersion CHAR(40) NOT NULL,           -- GIT commit hash for version of code used
+    progBuildTS FLOAT(53) NOT NULL,          -- timestamp of binary for this program; unix-style:
+                                             -- seconds since 1 Jan 1970 GMT; NULL means not
+                                             -- transferred
+    tsMotus FLOAT(53),                       -- timestamp when this record transferred to motus;
+                                             -- unix-style: seconds since 1 Jan 1970 GMT; NULL means not
+                                             -- transferred
+    PRIMARY KEY (batchID, progName)          -- only one version of a given program per batch
+);
+
+
+--  TABLE batchSWParams
+--
+--  maintains information about the parameter values used by programs
+--  in a batch run.
+--
+
+CREATE TABLE batchSWParams (
+    batchID INT NOT NULL REFERENCES batches, -- unique identifier of batch for this run
+    progName VARCHAR(16) NOT NULL,           -- identifier of program; e.g. "find_tags", "lotek-plugins.so"
+    parName varchar(16) NOT NULL,            -- name of parameter (e.g. "--minFreq")
+    parVal FLOAT(53) NOT NULL,               -- value of parameter
+    tsMotus FLOAT(53),                       -- timestamp when this record transferred to motus;
+                                             -- unix-style: seconds since 1 Jan 1970 GMT; NULL means not
+                                             -- transferred
+    PRIMARY KEY (batchID, progName, parName) -- only one value of a given parameter per program per batch
 );
 
 --  TABLE paramInfo
