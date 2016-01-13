bd91c2f8118c554317cf4c46f59b164bab6484f6
john brzustowski <jbrzusto@fastmail.fm>
2016-01-13 15:51:00 +0000

    * sgRunStream.R: 	- reorder params so mbn (which is optional) comes after f 	- allow integer vector (not just scalar) for mbn 	- return a list with one element per stream

--- package/R/sgRunStream.R
+++ package/R/sgRunStream.R
@@ -1,20 +1,18 @@
-#' Run a function on a single stream of raw data.
+#' Run a function on one or more streams of raw data.
 #'
-#' A stream of data consists of all records from raw files with the
-#' same monoBN (montonic boot number), ordered by increasing date.
-#' This function lets you run a function on one stream, specified by
-#' monoBN.  The stream is broken into chunks corresponding to the
-#' original files.  Each chunk is passed as a character scalar which
-#' will usually consist of multiple lines, separated by \code{'\n'}.
+#' A stream of raw data consists of all records from raw files with
+#' the same monoBN (montonic boot number), ordered by increasing date.
+#' This function lets you run a function on one or more streams,
+#' specified by monoBN, or all streams.  Each stream is broken into
+#' chunks corresponding to the original files.  Each chunk is passed
+#' as a character scalar which will usually consist of multiple lines,
+#' separated by \code{'\n'}.
 #' 
 #' @param src dplyr src_sqlite to receiver database
 #' 
-#' @param mbn integer monotonic boot number; this is the monoBN field
-#'     from the \code{files} table in the receiver's sqlite database.
-#' 
 #' @param f function with this signature:
 #' \enumerate{
-#' \item mbn integer monotonic boot number; same value passed to \code{sgRunStream}
+#' \item mbn integer monotonic boot number
 #' \item fts numeric file timestamp; the timestamp encoded in the file name
 #' \item cno chunk number, as follows:
 #' \enumerate{
@@ -30,35 +28,20 @@
 #' Typically, this would be an environment or reference class object.
 #' }
 #'
+#' @param mbn integer vector of monotonic boot numbers; this is the
+#'     monoBN field from the \code{files} table in the receiver's
+#'     sqlite database.  Defaults to NULL, which means run all streams
+#'     in order.
+#' 
 #' @param user arbitrary object passed to \code{f} as \code{user}
 #'     parameter on each call.  Defaults to NULL.
 #' 
-#' @return the valued returned by \code{f} when called with
-#'     \code{state=2L}; i.e. after \code{f} has been called with every
-#'     chunk.  All previous return values of \code{f} are discarded.
-#'     Any side-effects or accumulation of return value must be
-#'     performed by \code{f}.
-#'
-#' @examples
-#' 
-#' ##  u = new.env(emptyenv())
-#' ##  a = sgRunStream(
-#' ##       s2,
-#' ##       18L,
-#' ##       function(bn, ts, cno, ct, u) {
-#' ##         if (cno < 0){
-#' ##            u$ct=vector("list", -cno)
-#' ##         } else if (cno > 0) {
-#' ##            u$ct[[cno]] = unlist(stri_extract_all_regex(ct, "^G,[-,\\.0-9E]+$", opts_regex=list(multiline=TRUE), omit_no_match=TRUE))
-#' ##         } else {
-#' ##            return(u$ct)
-#' ##         }
-#' ##       },
-#' ##       u
-#' ##     )
-#' ##  g = stri_split_regex(unlist(u$ct), "[,\n]", simplify=TRUE, omit_no_match=TRUE)[,-1]
-#' ##  class(g) = "numeric" ## preserve dimensions
-#' 
+#' @return a list with one element per stream; the element is the
+#'     valued returned by \code{f} when called with \code{state=2L};
+#'     i.e. after \code{f} has been called with every chunk for the
+#'     given stream.  All previous return values of \code{f} are
+#'     discarded.  Any side-effects or accumulation of return value
+#'     must be performed by \code{f}.
 #'
 #' @export
 #' 
@@ -66,50 +49,64 @@
 #'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-sgRunStream = function(src, mbn, f, user=NULL) {
+sgRunStream = function(src, f, mbn=NULL, user=NULL) {
     sgEnsureDBTables(src)
     
-    if (! is.integer(mbn))
-        stop("mbn must be an integer")
+    if (! (is.null(mbn) || is.integer(mbn)))
+        stop("mbn must be NULL or an integer vector")
     if (!is.function(f) || length(formals(f)) != 5)
         stop("f must be a function accepting 5 parameters")
 
-    ## use low-level DBI functions for speed
+    ## by default, no filtering by mbn
+    where = ""
+    if (! is.null(mbn))
+        where = sprintf("where monoBN in (%s)", paste(mbn, collapse=","))
+    
     con = src$con
     n = dbGetQuery(
         con,
-        sprintf(
-            "select count(*) from files where monoBN=%d",
-            mbn)
-    ) [[1, 1]]
+        paste("select monoBN, count(*) as count from files", where, "group by monoBN order by monoBN")
+    )
 
-    if (n == 0L) {
-        warning("No files for receiver have monoBN ==", mbn)
+    if (sum(n) == 0L) {
+        warning("No files for receiver", if (! is.null(mbn)) " with specified monoBN")
         return(NULL)
     }
 
-    res <- dbSendQuery(
-        con,
-        sprintf(
-            "select ts, contents from files where monoBN=%d order by ts",
-            mbn)
-        )
+    rv = vector("list", nrow(n))
+    
+    for(i in 1:nrow(n)) {
+        mbn = n$monoBN[i]
+        count = n$count[i]
+        if (count > 0) {
+            ## use low-level DBI functions for speed and so we don't have to read
+            ## everything into memory at once
+            
+            res = dbSendQuery(
+                con,
+                sprintf("select ts, contents from files where monoBN==%d order by ts", mbn)
+            )
 
-    f(mbn, NULL, -n, NULL, user) ## allow user function to initialize
-    
-    i = 1L
-    while (i <= n) {
-        chunk = dbFetch(res, 1)
-        f(
-            mbn,
-            chunk$ts[[1]],
-            i,
-            chunk$contents[[1]] %>% memDecompress("bzip2", asChar=TRUE),
-            user
-        )
-        i = i + 1L
-     }
-    dbClearResult(res)
-    
-    return (f(mbn, NULL, 0L, NULL, user)) ## allow user function to finalize
+            f(mbn, NULL, -count, NULL, user) ## initialize new stream
+            j = 1L
+            while (j <= count) {
+                chunk = dbFetch(res, 1)
+                f(
+                  mbn,
+                  chunk$ts[[1]],
+                  j,
+                  if (length(chunk$contents[[1]]) > 0)
+                      chunk$contents[[1]] %>% memDecompress("bzip2", asChar=TRUE)
+                  else
+                      ""
+                 ,
+                  user
+                )
+                j = j + 1L
+            }
+            rv[[i]] = f(mbn, NULL, 0L, NULL, user)
+            dbClearResult(res)
+        }
+    }
+    return (rv) ## finalize last stream
 }
