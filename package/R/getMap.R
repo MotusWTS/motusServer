3ca710dd274d3ed11dc2c457c65054805fb25c27
john brzustowski <jbrzusto@fastmail.fm>
2016-03-18 19:38:24 +0000

    motusMap: tiny class to implement a persistent key:val map in .sqlite

---
+++ package/R/getMap.R
@@ -0,0 +1,30 @@
+#' Access (maybe after creating) a persistent named list in a database.
+#'
+#' This function creates an object of class motusMap, which behaves
+#' like a named list, allowing access via the \code{$} and \code{[[
+#' ]]} operators.  Elements assigned to the list are immediately
+#' stored in table \code{name} in the database pointed to by
+#' \code{src}.  An element can be removed from the list by assigning
+#' NULL to it.
+#'
+#' @param src dplyr src_sqlite to a database
+#'
+#' @param name name of table in database.
+#'
+#' @return an object of class "motusMap".
+#'
+#' @examples
+#'
+#' x = motusMap(src_sqlite("SG-1234BBBK5678.motus"), "meta")
+#' x$recvSerno
+#' x$MACAddr <- "01235a3be098"
+#' x$recvSerno <- NULL
+#' x[["MACAddr"]]
+
+getMap = function(src, name) {
+    con = src$con
+    if (! dbExistsTable(con, name))
+        dbGetQuery(con, sprintf("create table '%s' (key text primary key, val text);", name))
+    return(structure(paste("Map", name), name=name, con=con, class="motusMap"))
+}
+
