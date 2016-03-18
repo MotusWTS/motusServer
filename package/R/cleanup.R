92c9cac6b29d0c1ae3a3db96fdd896161b18eeab
john brzustowski <jbrzusto@fastmail.fm>
2016-03-18 18:06:31 +0000

    cleanup(): new arg dropTables boolean; drops rather than empty tables so they can be recreated to match a new schema

--- package/R/cleanup.R
+++ package/R/cleanup.R
@@ -5,14 +5,20 @@
 #'
 #' @param src dplyr src_sqlite to receiver database
 #'
+#' @param dropTables boolean; if TRUE, erase all output tables, rather
+#' than just emptying them.  This makes sense if their schema
+#' has changed, but is usually not required, so the default is FALSE.
+#'
 #' @export
 #' 
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-cleanup = function(src) {
+cleanup = function(src, dropTables = FALSE) {
     sql = function(...) dbGetQuery(src$con, sprintf(...))
-    for (t in c("batches", "runs", "hits", "batchParams", "batchProgs", "batchState"))
-        sql("delete from %s", t)
+    for (t in c("batches", "runs", "hits", "batchParams", "batchProgs", "batchState", "gps"))
+        sql("%s %s", if (dropTables) "drop table if exists" else "delete from", t)
+    if (dropTables)
+        sgEnsureDBTables(src)
     sql("vacuum")
 }
 
