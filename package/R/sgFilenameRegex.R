64bb6ec75fe8ed44bb600cf829594bbb6ee36daf
john brzustowski <jbrzusto@fastmail.fm>
2016-07-20 12:14:45 +0000

    - allow A..Z in BBBK serial number, instead of just digits

--- package/R/sgFilenameRegex.R
+++ package/R/sgFilenameRegex.R
@@ -1,5 +1,5 @@
 #' Regular expression for parsing data filenames from a sensorgnome receiver.
 #'@export
 
-sgFilenameRegex = "^(?<prefix>[^-]+)-(?<serno>[0-9]+BB(BK)?[0-9]+)-((?<macAddr>[0-9a-f]{12})-)?(?<bootnum>[0-9]+)-(?<ts>[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}-[0-9]{2}-[0-9]{2}(.[0-9]+)?)(?<tsCode>[P-Z])-(?<port>[a-z]+)(?<extension>\\.[a-z]+)(?<comp>\\.(gz|lz|bz2))?$"
+sgFilenameRegex = "^(?<prefix>[^-]+)-(?<serno>[0-9A-Z]+BB(BK)?[0-9A-Z]+)-((?<macAddr>[0-9a-f]{12})-)?(?<bootnum>[0-9]+)-(?<ts>[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}-[0-9]{2}-[0-9]{2}(.[0-9]+)?)(?<tsCode>[P-Z])-(?<port>[a-z]+)(?<extension>\\.[a-z]+)(?<comp>\\.(gz|lz|bz2))?$"
 
