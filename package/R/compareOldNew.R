0e67c5334e372eb431cc875f83fdbfa1312829b7
john brzustowski <jbrzusto@fastmail.fm>
2016-08-23 18:45:32 +0000

    plot comparisons even for sites in PRIVATE projects.  Closes #28

--- package/R/compareOldNew.R
+++ package/R/compareOldNew.R
@@ -13,7 +13,11 @@
 #'
 #' @param newSym - plot symbol for new detections; default: 24 (upward triangle)
 #'
-#' @return a character vector of full paths to any files created (plots, datasets)
+#' @return a character vector of full paths to any files created (plots, datasets);
+#' a vector of length zero if no plots or datasets are produced.
+#'
+#' @note: For projects marked "PRIVATE", this function uses both private and public
+#' tag detections, so results should be treated appropriately.
 #'
 #' @export
 #'
@@ -23,6 +27,8 @@ compareOldNew = function(year, proj, site, oldSym = 25, newSym = 24) {
     rv = character(0)
 
     f = sprintf("/SG/contrib/%d/%s/%s/%d_%s_%s_alltags.sqlite", year, proj, site, year, proj, site)
+    private = file.exists(sprintf("/SG/contrib/%d/%s/PRIVATE", year, proj))
+
     dbf = sub("_alltags", "", f)
 
     ylo = as.numeric(ymd_h(paste(year, 1, 1, 1, sep="-")))
@@ -30,34 +36,38 @@ compareOldNew = function(year, proj, site, oldSym = 25, newSym = 24) {
 
     ## get old dataset from either .rds or .sqlite files:
 
-    if (! file.exists(f)) {
-        f = sub(".sqlite$", ".rds", f)
-        if (! file.exists(f))
-            stop("Non-existent combination: ", year, proj, site)
-        old = readRDS(f)
-        if (length(old) == 0 || nrow(old) == 0)
-            stop("No tags in: ", year, proj, site)
-        old$ts = as.numeric(old$ts)
-        old$fullID = as.character(old$fullID)
-        old$ant = as.integer(as.character(old$ant))
-        old = old %>% as.tbl
-        told = old %>% mutate(hourBin=round(ts/3600-0.5, 0)) %>% group_by(recv, ant, fullID, hourBin) %>%
-        filter_ (~runLen >= 3 & (is.na(freqsd) | freqsd < 0.1) & ts >= ylo & ts <= yhi) %>%
-        summarize(n=length(ts), ts=min(ts), freq=mean(freq), sig=max(sig)) %>%
-        collect %>% as.data.frame
-    } else {
-        src = src_sqlite(f)
-        if (! "tags" %in% src_tbls(src))
-            stop("Database has no tags table for: ", year, proj, site)
-        old = tbl(src, "tags")
-        if (old %>% head(1) %>% collect %>% nrow != 1)
-            stop("No tag detections for: ", year, proj, site)
-        told = old %>% mutate(hourBin=round(ts/3600-0.5, 0)) %>% group_by(recv, ant, fullID, hourBin) %>%
-        filter_ (~runLen >= 3 & (is.na(freqsd) | freqsd < 0.1) & ts >= ylo & ts <= yhi) %>%
-        summarize(n=length(ts), ts=min(ts), freq=avg(freq), sig=max(sig)) %>%
-        collect %>% as.data.frame
-        ## if (told %>% head(1) %>% collect %>% nrow != 1)
-        ##     stop("No filtered tag detections for: ", year, proj, site, "\nPerhaps receiver clock was not correctly set?")
+    ff = f
+    if (private) {
+        ff = c(f, sub("alltags", "private_alltags", f, fixed=TRUE))
+    }
+
+    old = NULL
+    told = NULL
+
+    for (f in ff) {
+        if (! file.exists(f)) {
+            f = sub(".sqlite$", ".rds", f)
+            if (! file.exists(f))
+                stop("Non-existent combination: ", year, proj, site)
+            old = readRDS(f)
+            old$ts = as.numeric(old$ts)
+            old$fullID = as.character(old$fullID)
+            old$ant = as.integer(as.character(old$ant))
+            old = old %>% as.tbl
+        } else {
+            src = src_sqlite(f)
+            if (! "tags" %in% src_tbls(src)) {
+                old = NULL
+                warning("Database has no tags table for: ", year, proj, site)
+            } else {
+                old = tbl(src, "tags")
+            }
+        }
+        old = old %>% mutate(hourBin=round(ts/3600-0.5, 0)) %>% group_by(recv, ant, fullID, hourBin) %>%
+            filter_ (~runLen >= 3 & (is.na(freqsd) | freqsd < 0.1) & ts >= ylo & ts <= yhi) %>%
+            summarize(n=length(ts), ts=min(ts), freq=mean(freq), sig=max(sig)) %>%
+            collect %>% as.data.frame
+        told = rbind(told, old)
     }
 
     n2014map = list(
@@ -72,24 +82,26 @@ compareOldNew = function(year, proj, site, oldSym = 25, newSym = 24) {
         "Taylr"   = "Taylor"
     )
 
-    for (old in names(n2014map))
-        told$fullID = sub(old, n2014map[[old]], told$fullID, fixed=TRUE)
+    if (isTRUE(nrow(told) > 0)) {
+        for (old in names(n2014map))
+            told$fullID = sub(old, n2014map[[old]], told$fullID, fixed=TRUE)
 
-    told$new = rep(FALSE, nrow(told))
+        told$new = rep(FALSE, nrow(told))
 
-    ## generate physID of form ID:BI@FREQ
+        ## generate physID of form ID:BI@FREQ
 
-    told$physID = stri_replace_all_regex(told$fullID, "(.*)#(.*)@(.*):(.*)", "$2:$4@$3")
+        told$physID = stri_replace_all_regex(told$fullID, "(.*)#(.*)@(.*):(.*)", "$2:$4@$3")
 
-    ## re-arrange components of fullID to: ID:BI#PROJ@FREQ
+        ## re-arrange components of fullID to: ID:BI#PROJ@FREQ
 
-    told$fullID = stri_replace_all_regex(told$fullID, "(.*)#(.*)@(.*):(.*)", "$2:$4#$1@$3")
+        told$fullID = stri_replace_all_regex(told$fullID, "(.*)#(.*)@(.*):(.*)", "$2:$4#$1@$3")
 
-    ## for each receiver in the old form site database, get the serial
-    ## number and range of dates.  A site in a given year might have used
-    ## several different receivers.  A single receiver might appear in
-    ## the deployments table multiple times, once for each different
-    ## file prefix set by the user via the "shortLabel" field in deployment.txt
+        ## for each receiver in the old form site database, get the serial
+        ## number and range of dates.  A site in a given year might have used
+        ## several different receivers.  A single receiver might appear in
+        ## the deployments table multiple times, once for each different
+        ## file prefix set by the user via the "shortLabel" field in deployment.txt
+    }
 
     if (file.exists(dbf)) {
         olds = src_sqlite(dbf)
@@ -146,8 +158,11 @@ compareOldNew = function(year, proj, site, oldSym = 25, newSym = 24) {
     allnew$fullID = stri_replace_all_regex(allnew$fullID, "(.*)#(.*):(.*)@(.*)", "$2:$3#$1@$4")
 
     all = rbind(told, allnew)
+    if (nrow(all) == 0) {
+        warning("No detections in either new or old methods")
+        return(rv)
+    }
     class(all$ts) = c("POSIXt", "POSIXct")
-
 
     dayseq = seq(from=round(min(all$ts), "days"), to=round(max(all$ts),"days"), by=24*3600)
 
