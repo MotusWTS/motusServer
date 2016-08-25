9b0f86c6b88d6949f55eea9d1f838ec9ac3251d0
john brzustowski <jbrzusto@fastmail.fm>
2016-08-25 17:14:55 +0000

    fix to previous buggy version of serial number fix for Lotek receivers

--- R/getMotusMetaDB.R
+++ R/getMotusMetaDB.R
@@ -183,7 +183,7 @@ getMotusMetaDB = function() {
     }
     recv = recv %>% as.data.frame
     ## workaround until upstream changes format of serial numbers for Lotek receivers
-    recv$serno = sub("(SRX600|SRX800|SRX-DL)", "Lotek", perl=TRUE, t$serno)
+    recv$serno = sub("(SRX600|SRX800|SRX-DL)", "Lotek", perl=TRUE, recv$serno)
     dbWriteTable(s$con, "recvDeps", recv, overwrite=TRUE)
     dbWriteTable(s$con, "antDeps", ant %>% as.data.frame, overwrite=TRUE)
 
