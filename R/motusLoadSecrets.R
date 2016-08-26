bc673f48d2d50d171914893315ae0dec36a1fd62
john brzustowski <jbrzusto@fastmail.fm>
2016-08-26 19:49:37 +0000

    deal correctly with missing motus secrets file

--- R/motusLoadSecrets.R
+++ R/motusLoadSecrets.R
@@ -8,6 +8,8 @@
 #'   \item passwd: password at motus-wts.org
 #'   \item serno: serial number of a sensorgnome
 #' }
+#' This defaults to the file \code{motusSecrets.json} installed
+#' with this package.
 #'
 #' @param quiet if TRUE, return silently on failure; otherwise,
 #' report errors.
@@ -18,19 +20,16 @@
 #' 
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-motusLoadSecrets = function(f, quiet=FALSE) {
-    if (missing(f)) {
-        f = system.file("motusSecrets.json", package="motus")
-        if (! file.exists(f)) {
-            if (quiet)
-                return(FALSE)
-            stop("The package file motusSecrets.json is not installed.
+motusLoadSecrets = function(f = system.file("motusSecrets.json", package="motus"), quiet=FALSE) {
+    if (! file.exists(f)) {
+        if (quiet)
+            return(FALSE)
+        stop("The package file motusSecrets.json is not installed.
 You will not be able to use most motus functions without the credentials
 provided in this file.
 
 After receiving the file, use the function motusLoadSecrets(f) to load
 the secrets for a session, and saveSecrets() to store them permanently.")
-        }
     }
     MOTUS_SECRETS <<- f %>% readLines %>% paste(collapse="\n") %>% fromJSON
     return (TRUE)
