d5a03b1f9765c220995c3123d5c0ef88a136fdc8
john brzustowski <jbrzusto@fastmail.fm>
2016-08-29 19:36:33 +0000

    safe wrapper for system2()

---
+++ R/safeSys.R
@@ -0,0 +1,23 @@
+#' call a shell command safely.
+#'
+#' Quotes all parameters for a bash-type shell then runs the command with
+#' them using \link{\code{system2}}
+#'
+#' @param cmd full path to the executable file (can be a shell script,
+#'     for example)
+#'
+#' @param ... list of parameters to the command; these are combined
+#'     using \code{c()} and then quoted using \link{\code{shQuote()}}
+#'
+#' @return no return value.
+#'
+#' @note
+#'
+#' @export
+#'
+#' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
+
+safeSys = function(cmd, ...) {
+    ## Note: cmd is already quoted by system2(), but for some reason args are not
+    system2(cmd, shQuote(c(...)))
+}
