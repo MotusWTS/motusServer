026e7dfac08e662bc8559b57cca8f7da8572cf77
john brzustowski <jbrzusto@fastmail.fm>
2016-08-29 19:38:54 +0000

    receive emails directly into the /sgm/incoming folder

--- inst/scripts/incomingEmail.sh
+++ inst/scripts/incomingEmail.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 umask 0002
 
+## Emails are saved uncompressed into the queue directory /sgm/incoming
+## a server() function from the motus package handles further processing
+## when it detects a file has been written there.
+## Emails are recognized by their filename format:
+##
+##   msg_YYYY-MM-DDTHH-MM-SS.SSSSSSSSS
+##
+
 DATE=`date -u +%Y-%m-%dT%H-%M-%S.%N`
-DEST=/sgm/emails/msg_$DATE
+DEST=/sgm/incoming/msg_$DATE
 LOGFILE=/sgm/logs/emails.log.txt
 
 echo Got message $DATE >> $LOGFILE
 
 ## Save it to a file.
 /bin/cat > $DEST
-
-## run the processing code on it
-OUTFILE=$(tempfile)
-
-/sgm/bin/processMessage.py $DEST > $OUTFILE 2>&1
-cat $OUTFILE >> $LOGFILE
-
-## send a copy with the same subject line to admin user
-SUBJECT=$(grep -m 1 ^Subject: $DEST)
-cat $OUTFILE | mail -s "$SUBJECT" jbrzusto@fastmail.fm
-rm -f $OUTFILE
-
-## archive it
-lzip $DEST
