e99b98eded68cc2a840b534abe737bb9e452811d
john brzustowski <jbrzusto@fastmail.fm>
2016-08-29 19:37:28 +0000

    use outer join on ports to show connected units even if they have no entry in the serial number -> Project/Site map

--- inst/scripts/sgwho.R
+++ inst/scripts/sgwho.R
@@ -26,7 +26,7 @@ RES = unlist(dbGetQuery(CON, QUERY))
 
 cat(paste0("Streaming or Registering:\n", paste0(RES, collapse="\n"), "\n"))
 
-QUERY=paste0("select printf('%s,%5d,%s/%s', t1.Serno, t3.tunnelport, t1.Project, t1.Site) from map as t1 left join map as t2 on t1.Serno=t2.Serno and t1.tsHi < t2.tsHi join d.receivers as t3 on 'SG-' || t3.serno = t1.Serno where t2.tsHi is NULL and t3.tunnelport in (", paste0(PORTS, collapse=","), ")")
+QUERY=paste0("select printf('%s,%5d,%s/%s', 'SG-' || t3.serno, t3.tunnelport, t1.Project, t1.Site) from d.receivers as t3 left join map as t1  on 'SG-' || t3.serno = t1.Serno  left join map as t2 on t1.Serno=t2.Serno and t1.tsHi < t2.tsHi where t2.tsHi is NULL and t3.tunnelport in (", paste0(PORTS, collapse=","), ")")
 
 RES = unlist(dbGetQuery(CON, QUERY))
 cat(paste0("\nTunnel Ports:\n", paste0(RES, collapse="\n"), "\n"))
