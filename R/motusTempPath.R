af91d09c2e869a6c61859e664e9ee2e96e9e11b4
john brzustowski <jbrzusto@fastmail.fm>
2016-09-06 19:13:32 +0000

    centralize temporary file/dir creation in new function

---
+++ R/motusTempPath.R
@@ -0,0 +1,24 @@
+#' Create a temporary filename or folder for the motus server.
+#'
+#' @param isdir boolean scalar: if TRUE, a temporary folder is created; otherwise,
+#' a file is created
+#'
+#' @return invisible(NULL)
+#'
+#' @seealso \link{\code{server}}
+#' 
+#' @export
+#'
+#' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
+
+motusTempPath = function(isdir) {
+    
+    tmpd = tempfile(tmpdir=MOTUS_PATH$TMP)
+    if (info$isdir) {
+        dir.create(tmpd, mode=MOTUS_DEFAULT_FILEMODE)
+    } else {
+        close(file(tmpd, "wb"))
+        Sys.chmod(tmpd, mode=MOTUS_DEFAULT_FILEMODE)
+    }
+    invisible(NULL)
+}
