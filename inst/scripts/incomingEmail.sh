12f2951852ff0f7a202fa2abae92c7942fa3817d
john brzustowski <jbrzusto@fastmail.fm>
2016-09-16 19:40:20 +0000

    pipe incoming message to a file in /sgm/tmp, then move it into /sgm/incoming.  Prevents server seeing two events on the file (create, close)

--- inst/scripts/incomingEmail.sh
+++ inst/scripts/incomingEmail.sh
@@ -10,10 +10,17 @@ umask 0002
 ##
 
 DATE=`date -u +%Y-%m-%dT%H-%M-%S.%6N`
-DEST=/sgm/incoming/${DATE}_msg
+DEST=/sgm/tmp/${DATE}_msg
+INCOMING=/sgm/incoming
 LOGFILE=/sgm/logs/emails.log.txt
 
 echo Got message $DATE >> $LOGFILE
 
 ## Save it to a file, dropping CR
 /bin/cat | tr -d '\r' > $DEST
+
+## Move it (once complete) to the incoming folder
+## We do this in two steps so as not to trigger two separate
+## events per email, one on file creation, one on file close.
+
+/bin/mv $DEST $INCOMING
