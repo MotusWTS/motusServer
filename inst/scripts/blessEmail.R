ae13399a4e54c7e7c1623d72e16ab5c41149ccfc
john brzustowski <jbrzusto@fastmail.fm>
2016-09-19 02:00:44 +0000

    admin script to reprocess an email after adding an authorization token. Use only with emails which have been manually checked.

---
+++ inst/scripts/blessEmail.R
@@ -0,0 +1,47 @@
+#!/usr/bin/Rscript
+
+._(` (
+   blessEmail.R MSG
+
+Add an authorization toekn to an email message and resubmit it to
+the motus processing queue.
+
+MSG is the full path to a stored email message, typically in /sgm/manual
+or /sgm/spam
+
+._(` )
+
+ARGS = commandArgs(TRUE)
+
+suppressMessages(suppressWarnings(library(motus)))
+
+MSG       = ARGS[1]
+
+if (is.na(MSG) || ! file.exists(MSG) || file.info(MSG)$isdir) {
+    stop("You must specify the full path to a single email message; e.g. in /sgm/spam/2016-09-07T20-52-30.535589_msg.bz2")
+}
+
+## decompress if necessary
+if (grepl("\\.bz2$", MSG, perl=TRUE)) {
+    system2("bunzip2", MSG)
+    MSG = sub("\\.bz2$", "", MSG, perl=TRUE)
+}
+
+## add a token to the message's subject line
+x = readLines(MSG)
+
+## get line with subject, creating new one if none found
+subj = grep("^Subject: ", x, perl=TRUE)[1]
+if (is.na(subj)) {
+    subj = 1
+    x = c("Subject: ", x)
+}
+
+tok = getUploadToken()
+
+x[subj] = paste(x[subj], tok$token)
+
+writeLines(x, MSG)
+
+toIncoming(MSG)
+
