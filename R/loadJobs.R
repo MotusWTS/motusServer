fc57cd30d666f1529e957f162ced1ea4a19f1b9a
john brzustowski <jbrzusto@fastmail.fm>
2016-10-12 18:43:18 +0000

    job generation now using Twigs, managed by Copse; incomplete implementation

---
+++ R/loadJobs.R
@@ -0,0 +1,14 @@
+#' load existing jobs from server database
+#'
+#' A global JOBS object is created, which manages jobs.
+#' It is populated from the "jobs" table in the server database.
+#'
+#' @return no return value.
+#'
+#' @export
+#'
+#' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
+
+loadJobs = function() {
+    Jobs <<- Copse(MOTUS_SERVER_DB, "jobs")
+}
--- R/newJob.R
+++ R/newJob.R
@@ -1,30 +1,39 @@
-#' create a new job.
+#' create a new job
 #'
-#' A new unique job # is generated, and a folder for it is added to /sgm/queue/0
-#' An entry in the server database is created.
+#' @details
+#'
+#' A job is a Twig object from the Copse created by
+#' \link{\code{loadJobs}}, and an associated folder named NNNNNNNN,
+#' where N is the job number padded on the left with zeroes to a
+#' length of 8 digits.  Files associated with the job are stored in
+#' the folder, while metadata are stored in the Twig object, which is
+#' maintained in persistent storage in the Copse's SQLite database.
+#' Jobs can have subjobs via the Twig's parent() relationship.
+#'
+#'
+#' @param path to the folder in which to create the new job's folder
 #'
 #' @param type character scalar name of job handler
 #'
-#' @param params R object representable in JSON
+#' @param params list of parameters for this job
 #'
-#' @return This function returns a named integer vector of length 1
+#' @param ...  additional named metadata for this job
+#'
+#' @param parent .parent job, if any
+#'
+#' @return A Twig object (see \link{\code{Copse}}) representing the job.
 #'     with class "motusJob".  The value is the job number, and the
 #'     name is the full path to the new folder.
 #'
 #' @export
 #'
+#' @seealso \link{\code{Copse}}
+#'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-newJob = function(type, params) {
-    ts = as.numeric(Sys.time())
-    json = unclass(toJSON(list(type=type, params=params), auto_unbox=TRUE, POSIXt="epoch", digits=I(18)))
-    sql = safeSQL(MOTUS_SERVER_DB)
-    sql("insert into jobs (ts, complete, info) values (:ts, 0, :info)",
-        ts=ts, info=json)
-    ## get the ID of the new job
-    id = sql("select last_insert_rowid()")[[1]]
-    sql(.CLOSE=TRUE)
-    np = file.path(MOTUS_PATH$QUEUE0, sprintf("%08d", id))
-    dir.create(np)
-    return(structure(id, names=np, class="motusJob"))
+newJob = function(path, type, params, ..., .parent=NULL) {
+    rv = newTwig(Jobs, type=type, params=params, ..., .parent=.parent)
+    np = file.path(path, sprintf("%08d", twigID(rv)))
+    rv$dir = np
+    return(rv)
 }
