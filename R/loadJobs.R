68f376225f3e5933bb71df7ceace0ef35f5ebd57
john brzustowski <jbrzusto@fastmail.fm>
2016-10-24 15:54:18 +0000

    allow loading all jobs from a queue, instead of having a specific type.

--- R/loadJobs.R
+++ R/loadJobs.R
@@ -1,4 +1,4 @@
-#' Load unfinished jobs from server database and enqueue them
+#' Load unfinished jobs from server database and enqueue them.
 #'
 #' A global object \code{Jobs} is created, which manages jobs.
 #' It is populated from the "jobs" table in the server database.
@@ -11,6 +11,11 @@
 #' \item done: integer; 0 if not attempted; negative for errors; +1
 #' for successfully finished
 #'
+#' \item queue: integer; the queue to which a job belongs; there is one queue
+#' per running processServer().  If queue==0, the job has been enqueued
+#' for processing but not yet claimed by one of the processServers().
+#' If queue is null, it is being handled by emailServer() or pathServer()
+#'
 #' \item path: full path to folder for job
 #'
 #' \item oldpath: full path to previous folder for job (permits
@@ -18,14 +23,15 @@
 #'
 #' }
 #'
-#' Other job items are stored in a JSON-encodded text field called
+#' Other job items are stored in a JSON-encoded text field called
 #' \code{data}.
 #'
-#' For any jobs which are not done and whose head job is of the specified
-#' type, we verify that the "path" field is correct, in case the
-#' server was interrupted while moving a job.
+#' For any jobs to be loaded we verify that the "path" field is
+#' correct, in case the server was interrupted while moving a job.
 #'
-#' @param headType only examine jobs whose head job is of this type.
+#' @param which character scalar or integer scalar.  If character, only
+#'     load jobs whose head job is of this type.  If integer, only
+#'     load jobs from that queue.
 #'
 #' @return no return value.  Jobs are enqueued in the global \code{MOTUS_QUEUE}.
 #'
@@ -33,13 +39,24 @@
 #'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-loadJobs = function(headType) {
+loadJobs = function(which) {
+    if (missing(which) || length(which) != 1 || ! (is.integer(which) || is.character(which)))
+        stop("Must specify 'which' as an integer queue number or character scalar job type")
+
+    ## connect the global Jobs object to the MOTUS_SERVER_DB's jobs table
     Jobs <<- Copse(MOTUS_SERVER_DB, "jobs", type=character(), done=integer(), queue=integer(), path=character(), oldpath=character())
 
-    ## get IDs of jobs with possibly incorrect paths.
+    ## get IDs of jobs of selected type
 
-    j = query(Jobs,
-              paste0("select t1.id from jobs as t1 left join jobs as t2 on t1.stump=t2.id where (t1.path is not null and t1.oldpath is not null) and (t1.done == 0) and ((t2.id is NULL and t1.type=='", headType,"') or t2.type=='", headType, "') order by t1.id"))[[1]]
+    if (is.character(which)) {
+        j = query(Jobs,
+                  paste0("select t1.id from jobs as t1 left join jobs as t2 on t1.stump=t2.id where (t1.path is not null and t1.oldpath is not null) and (t1.done == 0) and ((t2.id is NULL and t1.type=='", which,"') or t2.type=='", which, "') order by t1.id"))[[1]]
+    } else if (is.integer(which)) {
+        j = query(Jobs, paste0("select id from jobs where done=0 and queue=", which))
+    }
+    ## correct paths in case server was interrupted after recording new path but before moving job,
+    ## and enqueue jobs
+
     for (i in j) {
         if (! file.exists(Jobs[[i]]$path) && file.exists(Jobs[[i]]$oldpath))
             Jobs[[i]]$path = Jobs[[i]]$oldpath
