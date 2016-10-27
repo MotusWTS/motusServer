95f45bd3f4313b5f2ba90b37db17177cb76c231a
john brzustowski <jbrzusto@fastmail.fm>
2016-10-27 19:31:23 +0000

    change default parameters to match reincarnation of old timing model in jbrzusto/find_tags

--- R/ltDefaultFindTagsParams.R
+++ R/ltDefaultFindTagsParams.R
@@ -2,11 +2,13 @@
 #'
 #' These are:
 #' \itemize{
-#' \item   pulses_to_confirm = 8       number of pulses required from a tag before its identity is treated as confirmed
-#' \item   clock_fuzz        = 30      parts per million; maximum discrepancy between tag and receiver clock
-#' \item   use_events                  use the events table in the tag database, so that tags are only sought while alive
-#' \item   max_skipped_time  = 200     seconds; maximum amount of time between detections in a tag run
-#' \item   default_freq      = 166.38  MHz, default antenna frequency, when not provided in input
+#' \item   pulses_to_confirm      number of pulses required from a tag before its identity is treated as confirmed
+#' \item   pulse_slop             ms; maximum allowed slop in pulse timing within a burst
+#' \item   burst_slop             ms; maximum allowed slop for a burst interval
+#' \item   burst_slop_expansion   ms; amount by which burst slop is allowed to grow per missed burst
+#' \item   use_events             use the events table in the tag database, so that tags are only sought while alive
+#' \item   max_skipped_bursts     seconds; maximum amount of time between detections in a tag run
+#' \item   default_freq           MHz; default antenna frequency, when not provided in input
 #'}
 #'
 #' @export
@@ -16,9 +18,11 @@
 ltDefaultFindTagsParams =
     paste(c(
         "--pulses_to_confirm=8",
-        "--clock_fuzz=30",
+        "--pulse_slop=1.5",
+        "--burst_slop=4",
+        "--burst_slop_expansion=1",
         "--use_events",
-        "--max_skipped_time=200",
+        "--max_skipped_bursts=20",
         "--default_freq=166.38"
         ),
         collapse = " ")
--- R/sgDefaultFindTagsParams.R
+++ R/sgDefaultFindTagsParams.R
@@ -2,16 +2,18 @@
 #'
 #' These are:
 #' \itemize{
-#' \item   pulses_to_confirm = 8       number of pulses required from a tag before its identity is treated as confirmed
-#' \item   frequency_slop    = 0.5     kHz in allowed variation in offset frequency within a run
-#' \item   min_dfreq         = 0       kHz; minimum allowed offset frequency
-#' \item   max_dfreq         = 12      kHz; maximum allowed offset frequency
-#' \item   clock_fuzz        = 30      parts per million; maximum discrepancy between tag and receiver clock
-#' \item   use_events                  use the events table in the tag database, so that tags are only sought while alive
-#' \item   max_skipped_time  = 200     seconds; maximum amount of time between detections in a tag run
-#' \item   default_freq      = 166.38  MHz, default antenna frequency, when not provided in input
+#' \item   pulses_to_confirm        number of pulses required from a tag before its identity is treated as confirmed
+#' \item   frequency_slop           kHz in allowed variation in offset frequency within a run
+#' \item   min_dfreq                kHz; minimum allowed offset frequency
+#' \item   max_dfreq                kHz; maximum allowed offset frequency
+#' \item   pulse_slop               ms; maximum allowed slop in pulse timing within a burst
+#' \item   burst_slop               ms; maximum allowed slop for a burst interval
+#' \item   burst_slop_expansion     ms; amount by which burst slop is allowed to grow per missed burst
+#' \item   use_events               use the events table in the tag database, so that tags are only sought while alive
+#' \item   max_skipped_bursts       seconds; maximum amount of time between detections in a tag run
+#' \item   default_freq             MHz, default antenna frequency, when not provided in input
 #'}
-#' 
+#'
 #' @export
 #'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
@@ -22,9 +24,11 @@ sgDefaultFindTagsParams =
         "--frequency_slop=0.5",
         "--min_dfreq=0",
         "--max_dfreq=12",
-        "--clock_fuzz=30",
+        "--pulse_slop=1.5",
+        "--burst_slop=4",
+        "--burst_slop_expansion=1",
         "--use_events",
-        "--max_skipped_time=200",
+        "--max_skipped_bursts=20",
         "--default_freq=166.38"
         ),
         collapse = " ")
