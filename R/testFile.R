62845ffd6a1878b9c010ecb70cfd77afa5d2cdde
john brzustowski <jbrzusto@fastmail.fm>
2016-10-27 19:28:34 +0000

    if a sensorgnome .txt.gz file fails integrity test, check for accompanying .txt file; if present, the test failure is correct and just means the .txt.gz file was downloaded before the SG had finished writing it, which is typical.  Don't complain or hive away the file in this case.

--- R/testFile.R
+++ R/testFile.R
@@ -37,7 +37,16 @@ testFile = function(files) {
         if (grepl("\\.(bz2|gz|zip|7z)$", files[i], ignore.case=TRUE)) {
             ec = attr(safeSys("7z", "t", files[i], minErrorCode=255), "exitCode")
             if (ec != 0) {
-                rv[i] = 3;  ## file is corrupt archive
+                ## a .gz archive can be corrupt because the SG hasn't finished writing it out
+                ## yet; the evidence for this will be existince of a file with the same name but
+                ## without the .gz ending; we check for this, and don't mark the file as
+                ## corrupt in this situation. In principle, we could just leave it out,
+                ## but downstream code is set up to handle this, so we just let it do so.
+
+                if ( (! grepl("\\.gz$", files[i], ignore.case=TRUE))
+                    || (! file.exists(sub("\\.gz$", "", files[i], ignore.case=TRUE)))) {
+                    rv[i] = 3;  ## file is corrupt archive
+                }
                 next
             }
         } else if (grepl("\\.rar$", files[i], ignore.case=TRUE)) {
