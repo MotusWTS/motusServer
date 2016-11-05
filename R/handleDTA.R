23865008c9309e4f75ed60d1a06aec992a43df60
john brzustowski <jbrzusto@fastmail.fm>
2016-11-05 03:40:41 +0000

    refactor handleDTA to work with new job scheme, and only run the tag finder in the new way, via queued jobs of (new) type "findtagsLt"

--- R/handleDTA.R
+++ R/handleDTA.R
@@ -1,59 +1,39 @@
 #' handle a folder of .DTA files
 #'
-#' Called by \code{\link{server}} for a file or folder added
-#' to the queue.
+#' Called by \code{\link{processServer}} for a file or folder added
+#' to the queue.  Merges files into receiver DBs, then queues
+#' a job to run the tag finder on each of these.
 #'
-#' @param path the full path to the file or directory.  It is only
-#'     treated as a file of DTA files if it is a directory whose name
-#'     begins with "dta_"
+#' @param j, the job.
 #'
-#' @param isdir boolean; TRUE iff the path is a directory
+#' @return TRUE after queueing jobs
 #'
-#' @param params not used
-#'
-#' @return TRUE iff the .DTA files were successfully handled.
-#'
-#' @seealso \link{\code{server}}
+#' @seealso \link{\code{processServer}}
 #'
 #' @export
 #'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-handleDTA = function(path, isdir, params) {
-    if (! isdir)
-        return (FALSE)
+handleDTA = function(j) {
 
-    handled = TRUE
+    ## merge files into receiver database(s)
 
-    ## run files the new way
-    rv = ltRunNewFiles(path)
+    info = ltMergeFiles(j$dir) %>% arrange(serno) %>% group_by(serno)
 
-    ## log any errors
-    if (any(! is.na(rv$err))) {
-        motusLog("HandleDTA errors: %s", paste0("   ", rv$err[!is.na(rv$err)], collapse="\n"))
-        handled = FALSE
+    ## queue findtags subjobs for each receiver having data files with
+    ## new content
+
+    runReceiver = function(f) {
+        ## nothing to do if no new files to use
+
+        if (! any(f$dataNew)) {
+            jobLog(paste0("Receiver ", f$serno[1], ":  the .DTA files have no new data, so the tagfinder will not be run"))
+            return(0)
+        }
+        queueJob(newSubJob(j, "findtagsLt", serno=f$serno[1], tsStart=min(f$ts[f$dataNew])))
     }
 
-    ## try running the old way;
-    rv = cbind(rv, getYearProjSite(rv$serno, rv$ts))
+    info %>% do (ignore = runReceiver(.))
 
-    ## fix bad years; Lotek receivers don't always have correct dates
-
-    rv$year[rv$year < 2010] = year(Sys.time())
-
-    ## queue a reprocessing of each old site with the new files
-
-    ## function to handle files from one old site
-    queueOldSite = function(files) {
-        ## move files for this receiver to a new temp folder
-        tmpdir = makeQueuePath("dtaold", gsub('/', '%', fixed=TRUE, oldSitePath(files$year[1], files$proj[1], files$site[1])))
-        moveFiles(files$fullname, tmpdir)
-
-        ## name of dir in queue will be "TIMESTAMP_dtaold_%SG%YEAR%PROJ%SITE"
-        enqueue(tmpdir)
-    }
-
-    rv %>% group_by(year, proj, site) %>% do(ignore = queueOldSite(.))
-
-    return(handled)
+    return(TRUE)
 }
---
+++ R/handleFindtagsLt.R
@@ -0,0 +1,30 @@
+#' run the tag finder on all files from a Lotek receiver
+#'
+#' Called by \code{\link{processServer}}. After running the tag
+#' finder, queues a new subjob that exports data.
+#'
+#' @param j, the job.  It has these properties:
+#' \enumerate{
+#' \item serno: serial number of receiver; "Lotek-NNN"
+#' \item tsStart: earliest timestamp of a detection in a file
+#' with new data.
+#' }
+#'
+#' @return
+#'
+#' @seealso \link{\code{processServer}}
+#'
+#' @export
+#'
+#' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
+
+handleFindtagsLt = function(j) {
+
+    jobLog(paste0("Running tag finder on receiver ", j$serno))
+    rv = ltFindTags(sgRecvSrc(j$serno), getMotusMetaDB())
+    jobLog(paste0("Got ", rv, " tag detections."))
+
+    queueJob(newSubJob(topJob(j), "exportData", serno=j$serno, tsStart=j$tsStart))
+
+    return(TRUE)
+}
