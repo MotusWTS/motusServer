ae9033b2b9831267d53c39f590267b3918a2bb88
john brzustowski <jbrzusto@fastmail.fm>
2016-11-08 18:58:21 +0000

    add models 3, Z for raspberry pi (even though no software image for those yet).

--- R/sgFilenameRegex.R
+++ R/sgFilenameRegex.R
@@ -1,5 +1,4 @@
 #' Regular expression for parsing data filenames from a sensorgnome receiver.
 #'@export
 
-sgFilenameRegex = "^(?<prefix>[^-]+)-(?<serno>[0-9A-Z]{4}(RPi2|BBBK|BB)[0-9A-Z]{4,6}(_[0-9])?)-(?<bootnum>[0-9]+)-(?<ts>[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}-[0-9]{2}-[0-9]{2}(.[0-9]+)?)(?<tsCode>[P-Z])-(?<port>[a-z]+)(?<extension>\\.[a-z]+)(?<comp>\\.(gz|lz|bz2))?$"
-
+sgFilenameRegex = "^(?<prefix>[^-]+)-(?<serno>[0-9A-Z]{4}(RPi[23Z]|BBBK|BB)[0-9A-Z]{4,6}(_[0-9])?)-(?<bootnum>[0-9]+)-(?<ts>[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}-[0-9]{2}-[0-9]{2}(.[0-9]+)?)(?<tsCode>[P-Z])-(?<port>[a-z]+)(?<extension>\\.[a-z]+)(?<comp>\\.(gz|lz|bz2))?$"
