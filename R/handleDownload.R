69f9ab96fa46e93951324cd2bda90d783fb29e04
john brzustowski <jbrzusto@fastmail.fm>
2016-11-17 15:07:01 +0000

    if download fails, log that to job log, and leave job marked as not done.

--- R/handleDownload.R
+++ R/handleDownload.R
@@ -47,8 +47,18 @@ following the instructions here:\n
         return (FALSE)
     }
     jobLog(j, paste0("Downloading using method '", method, "' for:\n   ", sanURL))
-    jobLog(j, getter(url, path))
+    tryCatch ({
+        rv = getter(url, path)
+    }, error = function(e) {
+        e = paste0(e)
+        if (grepl("Not Found", e, ignore.case=TRUE)) {
+            e = paste("We failed to download the file or folder you sent.\nEither the link is wrong, or the content has expired.\nPlease try resending.\nCurl error message was: ", e)
+            jobLog(j, e)
+            return(FALSE)
+        }
+    })
 
+    jobLog(j, rv)
     newSubJob(j, "sanityCheck", dir=path)
     newSubJob(j, "queueArchives", dir=path)
 
