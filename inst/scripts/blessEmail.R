52451be31156b8a0ff0e210af558efcc11db6831
john brzustowski <jbrzusto@fastmail.fm>
2016-11-20 13:25:16 +0000

    allow specifiying sensorgnome.org user so that system emails will be routed to that them.

--- inst/scripts/blessEmail.R
+++ inst/scripts/blessEmail.R
@@ -1,13 +1,15 @@
 #!/usr/bin/Rscript
 
 ._(` (
-   blessEmail.R MSG
+   blessEmail.R MSG [USER]
 
-Add an authorization toekn to an email message and resubmit it to
+Add an authorization token to an email message and resubmit it to
 the motus processing queue.
 
 MSG is the full path to a stored email message, typically in /sgm/manual
 or /sgm/spam
+
+USER is the sg.org wiki username; if not specified, defaults to 'admin'
 
 ._(` )
 
@@ -16,10 +18,14 @@ ARGS = commandArgs(TRUE)
 suppressMessages(suppressWarnings(library(motus)))
 
 MSG       = ARGS[1]
+USER      = ARGS[2]
 
 if (is.na(MSG) || ! file.exists(MSG) || file.info(MSG)$isdir) {
     stop("You must specify the full path to a single email message; e.g. in /sgm/spam/2016-09-07T20-52-30.535589_msg.bz2")
 }
+
+if (is.na(USER))
+    USER = "admin"
 
 ## decompress if necessary
 if (grepl("\\.bz2$", MSG, perl=TRUE)) {
@@ -37,7 +43,7 @@ if (is.na(subj)) {
     x = c("Subject: ", x)
 }
 
-tok = getUploadToken()
+tok = getUploadToken(USER)
 
 x[subj] = paste(x[subj], tok$token)
 
