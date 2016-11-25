b9e894030b44c46cd89751fa687d061f37b72981
john brzustowski <jbrzusto@fastmail.fm>
2016-11-25 13:26:48 +0000

    new options for: - export only - cleanup before re-running to ease manual interventions

--- R/handleRerunReceiver.R
+++ R/handleRerunReceiver.R
@@ -1,8 +1,8 @@
-#' rerun data from a receiver
+#' re-run data from a receiver
 #'
 #' Called by \code{\link{processServer}}
 #'
-#' @details reruns the tagfinder for a receiver, possibly limiting the
+#' @details re-runs the tagfinder for a receiver, possibly limiting the
 #' re-run to one or more boot sessions (for SGs).
 #'
 #' @param j the job with these items:
@@ -12,8 +12,15 @@
 #' \item serno character scalar; the receiver serial number
 #'
 #' \item monoBN integer vector of length 2; the range of receiver boot
-#' sessions to run; NULL for Lotek receivers.  If not specified for an
-#' SG receiver, then all boot sessions are rerun.
+#' sessions to run; If not specified, then for an SG receiver, all
+#' boot sessions are rerun.  Ignored for Lotek receivers.
+#'
+#' \item exportOnly logical scalar; if TRUE, skip running the tagfinder
+#'
+#' \item cleanup logical scalar; if TRUE, delete all hits, runs,
+#' batches before re-running.  Ignored (and treated as FALSE) if
+#' monoBN is specified.  i.e. cleanup can only be specified for a
+#' full re-run.
 #'
 #' }
 #'
@@ -28,42 +35,59 @@
 handleRerunReceiver = function(j) {
     serno = j$serno
     monoBN = j$monoBN
+    exportOnly = j$exportOnly
+    cleanup = j$cleanup
+    if (length(monoBN) > 0)
+        cleanup = FALSE
 
     isLotek = grepl("^Lotek", serno, perl=TRUE)
 
+    ## Lock the receiver; this is really only needed for cleanup and
+    ## selecting existing boot sessions, but easiest to always do.
+
     while(! lockReceiver(serno)) {
-        ## FIXME: we should probably return NA immediately, and have processServer re-queue the job at the end of the queue
+        ## FIXME: we should probably return NA immediately, and have
+        ## processServer re-queue the job at the end of the queue
         Sys.sleep(10)
     }
 
-    ## make sure we unlock the receiver DB when this function exits, even on error
-    ## NB: the runMotusProcessServer script also drops any locks held by a given
-    ## processServer after the latter exits.
+    ## make sure we unlock the receiver DB when this function exits,
+    ## even on error.  NB: the runMotusProcessServer script also drops
+    ## any locks held by a given processServer after the latter exits.
 
     on.exit(lockReceiver(serno, FALSE))
 
-    ## function to queue a run of a receiver boot session, and export of its data
+    if (cleanup) {
+        cleanup(sgRecvSrc(serno), TRUE)
+    }
 
-    if (isLotek) {
-        newSubJob(j, "LtFindtags", serno=serno)
-    } else {
-        ## get all boot sessions within the range, or all if null
-        src = sgRecvSrc(serno)
-        allBN = dbGetQuery(src$con, "select distinct monoBN from files order by monoBN")[[1]]
-        if (is.null(monoBN)) {
-            monoBN = allBN
+    ## queue runs of a receiver (or some boot session(s), for SGs)
+
+    if (! exportOnly) {
+        if (isLotek) {
+            newSubJob(j, "LtFindtags", serno=serno)
         } else {
-            ## subset of the sequence monoBN[1]:monoBN[2] for which we have files
-            monoBN = allBN[allBN >= monoBN[1] & allBN <= monoBN[2]]
-        }
-        for (mbn in monoBN) {
-            newSubJob(j, "SGfindtags",
-                      serno = serno,
-                      monoBN = mbn,
-                      canResume = FALSE
-                      )
+            ## get all boot sessions within the range, or all if null
+            src = sgRecvSrc(serno)
+            allBN = dbGetQuery(src$con, "select distinct monoBN from files order by monoBN")[[1]]
+            if (length(monoBN) == 0) {
+                monoBN = allBN
+            } else {
+                ## subset of the sequence monoBN[1]:monoBN[2] for which we have files
+                monoBN = allBN[allBN >= monoBN[1] & allBN <= monoBN[2]]
+            }
+            for (mbn in monoBN) {
+                newSubJob(j, "SGfindtags",
+                          serno = serno,
+                          monoBN = mbn,
+                          canResume = FALSE
+                          )
+            }
         }
     }
+
+    ## queue data export
+
     newSubJob(topJob(j), "exportData", serno=serno)
     newSubJob(topJob(j), "oldExport", serno=serno, monoBN=range(monoBN))
 
--- inst/scripts/rerunReceiver.R
+++ inst/scripts/rerunReceiver.R
@@ -6,15 +6,23 @@
 
 ARGS = commandArgs(TRUE)
 
-if (! length(ARGS) %in% c(1, 3)) {
+if (length(ARGS) == 0) {
     cat("
 
-Usage: rerunReceiver.R SERNO [BLO BHI]
+Usage: rerunReceiver.R [-d] [-e] SERNO [BLO BHI]
 
- - for Lotek receivers, all raw data are reprocessed
+where:
 
- - for an SG, you can specify a range of boot sessions by specifying BLO and BHI
-   as the low and high boot sessions, respectively.
+ SERNO: receiver serial number; e.g. SG-0613BB000613 or Lotek-123
+
+ BLO BHI: for an SG, you can specify a range of boot sessions by specifying
+ BLO and BHI as the low and high boot sessions, respectively;
+ for Lotek receivers, all raw data are reprocessed
+
+ -e: don't re-run the tag finder; just re-export data
+
+ -d: before running the tag finder, delete existing batches, runs, hits for
+     the specified boot sessions
 
 A new job will be created and placed into the master queue (queue 0),
 from where a processServer can claim it.
@@ -24,6 +32,32 @@ from where a processServer can claim it.
     q(save="no", status=1)
 }
 
+exportOnly = FALSE
+cleanup = FALSE
+monoBN = NULL
+
+while(isTRUE(substr(ARGS[1], 1, 1) == "-")) {
+    switch(ARGS[1],
+           "-e" = {
+               exportOnly = TRUE
+           },
+           "-c" = {
+               cleanup = TRUE
+           },
+           {
+               stop("Unknown argument: ", ARGS[1])
+           })
+    ARGS = ARGS[-1]
+}
+
+serno = ARGS[1]
+if (is.na(serno)) stop("You must specify a receiver serial number.")
+
+ARGS = ARGS[-1]
+
+if (length(ARGS) > 0) {
+    monoBN = range(as.integer(ARGS[-1]))
+}
 
 suppressMessages(suppressWarnings(library(motus)))
 
@@ -31,11 +65,7 @@ suppressMessages(suppressWarnings(library(motus)))
 
 loadJobs()
 
-j = newJob("rerunReceiver", .parentPath=MOTUS_PATH$INCOMING, serno=ARGS[1], .enqueue=FALSE)
-
-if (length(ARGS) == 3) {
-    j$monoBN = as.integer(ARGS[-1])
-}
+j = newJob("rerunReceiver", .parentPath=MOTUS_PATH$INCOMING, serno=serno, monoBN=monoBN, exportOnly=exportOnly, cleanup=cleanup, .enqueue=FALSE)
 
 ## move the topJob to the top-level processServer queue
 
