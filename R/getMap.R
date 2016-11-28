f065125b84b2cda29743dc87fb9121af7be7c142
john brzustowski <jbrzusto@fastmail.fm>
2016-11-28 14:29:24 +0000

    - cleanup motusMap into single file - correct typo that made `[[<-.motusMap` a duplicate of `$<-.motusMap`

--- R/getMap.R
+++ R/getMap.R
@@ -1,6 +1,6 @@
 #' Access (maybe after creating) a persistent named list in a database.
 #'
-#' This function creates an object of class motusMap, which behaves
+#' This function creates an object of S3 class "motusMap", which behaves
 #' like a named list, allowing access via the \code{$} and \code{[[
 #' ]]} operators.  Elements assigned to the list are immediately
 #' stored in table \code{name} in the database pointed to by
@@ -28,4 +28,82 @@ getMap = function(src, name="meta") {
     if (! dbExistsTable(con, name))
         dbGetQuery(con, sprintf("create table '%s' (key text primary key, val text);", name))
     return(structure(paste("Map", name), name=name, con=con, class="motusMap"))
+}
+
+#' get the key values of a map table in a database
+#'
+#' @param map object of class "motusMap"
+#'
+#' @return character vector; all keys in the map
+#'
+#' @export
+
+names.motusMap = function(map) {
+    return (dbGetQuery(attr(map, "con"), sprintf("select key from %s", attr(map, "name")))[[1]])
+}
+
+#' set an element in a motus map
+#'
+#' Set the value associated with a key in the given map.
+#'
+#' @param map object of class "motusMap", as created by \code{motusMap}
+#'
+#' @param key key of item, as character scalar
+#'
+#' @param val character string value to associate with \code{key}
+#'
+#' @export
+#'
+
+`[[<-.motusMap` = function(map, key, value) {
+    if (! is.null(value))
+        dbGetPreparedQuery(attr(map, "con"), sprintf("insert or replace into %s (key, val) values (:key, :val)", attr(map, "name")), data.frame(key=key, val=value, stringsAsFactors=FALSE))
+    else
+        dbGetPreparedQuery(attr(map, "con"), sprintf("delete from %s where key=:key", attr(map, "name")), data.frame(key=key))
+    return(invisible(map))
+}
+
+#' set an element in a motus map
+#'
+#' Set the value associated with a key in the given map.
+#'
+#' @param map object of class "motusMap", as created by \code{motusMap}
+#'
+#' @param key key of item, as bare symbol
+#'
+#' @param val character string value to associate with \code{key}
+#'
+#' @export
+#'
+
+`$<-.motusMap` = function(map, key, value) {
+    `[[<-.motusMap`(map, substitute(key), value)
+}
+
+#' Return the value of an item from a motusMap.
+#'
+#' @param map object of class "motusMap"
+#'
+#' @param key character scalar; name of item in map
+#'
+#' @return character scalar; value associated with \code{key}.
+#'
+#' @export
+
+`[[.motusMap` = function(map, key) {
+    return (dbGetPreparedQuery(attr(map, "con"), sprintf("select val from %s where key=:key", attr(map, "name")), data.frame(key=key, stringsAsFactors=FALSE))[[1]])
+}
+
+#' Return the value of an item from a motusMap.
+#'
+#' @param map object of class "motusMap"
+#'
+#' @param key character scalar; name of item in map
+#'
+#' @return character scalar; value associated with \code{key}.
+#'
+#' @export
+
+`$.motusMap` = function(map, key) {
+    `[[.motusMap` (map, substitute(key))
 }
