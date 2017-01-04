8cafe24a1e3324e768d75b23e6ce97910af08f95
john brzustowski <jbrzusto@fastmail.fm>
2017-01-04 15:22:45 +0000

    propagate change of name for receiver serial number map db

--- inst/scripts/sgwho.R
+++ inst/scripts/sgwho.R
@@ -18,7 +18,7 @@ if (isTRUE(ARGS[1] == "-p")) {
 RECVS = paste0("SG-", sort(dir("/home/sg_remote/connections")))
 
 YEAR = strftime(Sys.time(), "%Y")
-CON = dbConnect(SQLite(), paste0("/SG/", YEAR, "_receiver_serno.sqlite"))
+CON = dbConnect(SQLite(), "/SG/receiver_map.sqlite")
 dbGetQuery(CON, "attach database '/home/sg_remote/receivers.sqlite' as d")
 
 QUERY=paste0("select printf('%s %s/%s', t1.Serno, t1.Project, t1.Site) from map as t1 left join map as t2 on t1.Serno=t2.Serno and t1.tsHi < t2.tsHi where t2.tsHi is NULL and t1.Serno in (", paste0("'", RECVS, "'", collapse=","), ")")
