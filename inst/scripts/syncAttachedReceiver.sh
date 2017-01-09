e8f6409199eebaeab1ddad3d396193eb50e6b592
john brzustowski <jbrzusto@fastmail.fm>
2017-01-09 20:38:07 +0000

    shell script to launch sync of receiver and queue next periodic launch
    This script is run (with only the serno paramter, to allow for a random
    initial delay) by /home/sg_remote/code/sg_remote when a receiver
    establishes a connection

--- inst/scripts/syncAttachedReceiver.sh
+++ inst/scripts/syncAttachedReceiver.sh
@@ -1,43 +1,51 @@
-#' Grab and process new files from an attached SG.
-#'
-#' Uses rsync to copy/update files from the receiver into the
-#' \code{MOTUS_PATH$FILE_REPO} folder, then queues a job to process
-#' these files.  This function can be called from a script that
-#' runs as a cron job, for example.
-#'
-#' @param serno receiver serial number
-#'     the full path to a directory, which will be searched
-#'     recursively for raw sensorgnome data files.
-#'
-#' @param dbdir path to folder with existing receiver databases
-#' Default: \code{MOTUS_PATH$RECV}
-#'
-#' @return a list with two items:
-#' \itemize{
-#' \item info- a data_frame reporting the details of each file, with these columns:
-#' \itemize{
-#' \item name - full path to filename
-#' \item use - TRUE iff data from this file will be incorporated in this run (i.e. the file is new, or has
-#' more content than its previous version)
-#' \item new  - TRUE iff a file of this name was not yet in database
-#' \item done  - TRUE iff this is a compressed file and we have its full contents
-#' \item corrupt      - TRUE the file was compressed but corrupted
-#' \item small - TRUE iff the file contents are shorter than existing contents for that file in the receiver DB
-#' \item partial - TRUE iff this is a partial compressed file for which an uncompressed version is in the same batch
-#' \item serno - serial number of the receiver, parsed from this filename
-#' \item monoBN - monotonic boot session, parsed from filename
-#' \item ts - starting timestamp, parsed from filename
-#' }
-#' \item resumable - a logical vector indicating the tag finder can be run with --resume for each
-#' subset of files from a distinct (serno, monoBN) pair.  The vector has names consisting of
-#' \code{paste(serno, monoBN)}.
-#' }
-#'
-#' Returns NULL if no valid sensorgnome data files were found.
-#'
-#' @export
-#'
-#' @seealso \code{sgRunNewFiles}, which calls this function and then calls \code{sgFindTags}
-#' as appropriate.
-#'
-#' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
+# Repeatedly trigger a job to grab and process new files from an attached SG.
+#
+# This script creates an empty file with a given SG serial number in /sgm/sync
+# The motusSyncServer detects this, then queues a job to sync that receiver.
+#
+# This script also creates an at job for itself, so that sync happens again
+#
+# Usage:
+#     syncAttachedReceiver.sh SERNO WAITLO WAITHI
+#
+# If the receiver with the serial number SERNO is indeed attached,
+# we create the empty /sgm/sync/SERNO using touch, generate a random
+# wait time between WAITLO and WAITHI minutes, then launch an at-job
+# for this script.
+#
+# If WAITLO or WAITHI are not specified, then defaults are used, and
+# the delay occurs *before* the sync job is launched for the first
+# time.
+
+SERNO=$1
+WAITLO=$2
+WAITHI=$3
+RUNNOW=1
+
+if [[ -f ~sg_remote/connections/${SERNO/SG-/} ]]; then
+    ## sanitize SERNO by removing everything but alphanumerics and '-'
+    SERNO=${SERNO//[^-[:alnum:]]/}
+
+    ## sanitize WAITLO, WAITHI by removing non-digits
+    WAITLO=${WAITLO//[^0-9]/}
+    WAITHI=${WAITHI//[^0-9]/}
+
+    ## by default, use WAITLO = 30 minutes, WAITHI = 90  (average of 1 hour)
+    if [[ "$WAITLO" == "" ]]; then
+        WAITLO=30
+        RUNNOW=""
+    fi
+    if [[ "$WAITHI" == "" ]]; then
+        WAITHI=90
+        RUNNOW=""
+    fi
+
+    WAIT=$(( $WAITLO + `/usr/bin/od -N 2 -t u2 -A n /dev/urandom` * ($WAITHI - $WAITLO) / 65535 ))
+
+    if [[ $RUNNOW ]]; then
+        ## trigger a sync job by the motusSyncServer
+        touch /sgm/sync/$SERNO
+    fi
+
+    echo $0 $SERNO $WAITLO $WAITHI | at -M now + $WAIT minutes
+fi
