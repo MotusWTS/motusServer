a57cbae6946470a25f01d6371a0ac111328b9bc3
john brzustowski <jbrzusto@fastmail.fm>
2017-01-12 16:47:05 +0000

    drop spurious 2nd 'drop table'

--- R/dbInsertOrReplace.R
+++ R/dbInsertOrReplace.R
@@ -1,11 +1,11 @@
-#' Save records from a data frame into a db table, replacing 
+#' Save records from a data frame into a db table, replacing
 #' existing records when primary keys collide.
 #'
 #' Seems like an obvious missing piece of the DBI.  Ideally,
 #' dbWriteTable would have a boolean 'replace' argument that served
 #' the same purpose; i.e. record-wise overwrite, rather than
 #' table-wise.
-#' 
+#'
 #' @param con DBI database connection
 #'
 #' @param name name of table to insert or replace records into
@@ -15,13 +15,13 @@
 #' @return no return value
 #'
 #' @export
-#' 
+#'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
 dbInsertOrReplace = function(con, name, df) {
     if (nrow(df) == 0)
         return()
-    
+
     sql = function(...) dbGetQuery(con, sprintf(...))
 
     tmp = basename(tempfile("zztmp"))
@@ -42,6 +42,5 @@ dbInsertOrReplace = function(con, name, df) {
 
     ## drop the temporary table
 
-    sql("drop table %s", tmp)
     sql("drop table %s", tmp)
 }
