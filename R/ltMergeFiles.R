5b3ebf3b9fd49caced81bc11e2c52993309c4663
john brzustowski <jbrzusto@fastmail.fm>
2017-01-13 13:24:25 +0000

    factor out determination of model from serial number into own function

--- R/ltMergeFiles.R
+++ R/ltMergeFiles.R
@@ -107,43 +107,7 @@ ltMergeFiles = function(files, dbdir=MOTUS_PATH$RECV) {
             meta$dbType = "receiver" ## indicate this is a receiver database (vs. a tagProject database)
             meta$recvSerno = x$recv
             meta$recvType = "Lotek"
-
-            ## get bare serial number by dropping "Lotek-"
-            bareno = sub("Lotek-", "", x$recv, fixed=TRUE)
-
-            ## map to model as per info from Lotek:
-
-            ## > Yes, serial number uniquely identifies receiver.  All the SRX600
-            ## > receiver serial numbers are 6###.  The SRX800 serial numbers start at 1.
-            ## > It will be a long time before we get to 6000.  The old SRX400A models
-            ## > were 9###A and up.
-            ## ---
-            ## > SRX-DL receivers have serial numbers 8###.
-
-            ## and further:
-
-            ## > As of June 1 2016 we decided to switch the SRX800 D
-            ## > variant SN allocation to the format, D######,
-            ## > i.e. D000426. This helps us distinguish a D variant
-            ## > from a M / MD variant. The change in SN allocation
-            ## > actually occurs from SN 000390 to SN D000391.
-
-            if (substr(bareno, 1, 1) == "D") {
-                model = "SRX800D"
-            } else if (bareno >= "9000A") {
-                model = "SRX400A"
-            } else if (bareno >= "8000") {
-                model = "SRX-DL"
-            } else if (bareno >= "6000") {
-                model = "SRX600"
-            } else if (as.integer(bareno) >= 391) {
-                ## per Lotek, it's not a model "D"
-                model = "SRX800M/MD"
-            } else {
-                model = "SRX800"
-            }
-
-            meta$recvModel = model
+            meta$recvModel = getLotekModel(x$recv)
 
             ## write file record
             dbGetPreparedQuery(
