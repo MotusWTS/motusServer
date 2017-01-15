c956605a869faf9656eb1dd4d28c868193ab48f8
john brzustowski <jbrzusto@fastmail.fm>
2017-01-15 13:55:09 +0000

    show job parameters in detail view

--- R/statusServer.R
+++ R/statusServer.R
@@ -143,20 +143,25 @@ for (var j=1; j <= numJobs; ++j) {
 dumpJobDetails = function(res, j, i) {
     j = Jobs[[j]]
     res$write(paste0('<div class="jobDetails jobDetails', i, '" style="display:none">'))
-    replyTo = paste(j$replyTo, collapse=", ")
+    info = blob(j)
+    replyTo = paste(info$replyTo, collapse=", ")
     if (is.null(replyTo))
         replyTo = "none"
 
-    log = j$log
+    log = info$log
+    summary = info$summary
+    info = info[! names(info) %in% c("log", "summary", "replyTo")]
+    params = paste(names(info), info, sep="=", collapse=", ")
     if (isTRUE(nchar(log) > 10000))
         log = paste0(substr(log, 1, 5000), "\n   ...\n", substring(log, nchar(log)-5000), "\n")
-    res$write(sprintf("<h3>Status for job %d</h3><pre><b>Created Date:</b> %s\n<b>Last Activity:</b> %s\n<b>Sender:</b> %s\n<b>Queue: </b>%s\n<b>Summary: </b>%s</pre><h4>Log:</h4><pre>%s\n</pre>",
+    res$write(sprintf("<h3>Status for job %d</h3><pre><b>Created Date:</b> %s\n<b>Last Activity:</b> %s\n<b>Sender:</b> %s\n<b>Parameters:</b> %s\n<b>Queue: </b>%s\n<b>Summary: </b>%s</pre><h4>Log:</h4><pre>%s\n</pre>",
                       j,
                       format(TS(ctime(j))),
                       format(TS(mtime(j))),
                       replyTo,
+                      params,
                       if (is.na(j$queue)) "None" else paste(j$queue),
-                      if (is.null(j$summary)) "" else j$summary,
+                      if (is.null(summary)) "" else summary,
                       paste0("   ", gsub("\n", "\n   ", log, fixed=TRUE))
                       )
               )
