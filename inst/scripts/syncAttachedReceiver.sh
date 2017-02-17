1bbec1e3155b9e1f406da1eacb6d9f2efd3eba10
john brzustowski <jbrzusto@fastmail.fm>
2017-02-17 20:18:07 +0000

    correct tunnel port for any receiver connecting that is known to have the wrong one. Closes #126 (at least once all receivers with "wrong" tunnel ports have connected.)

--- inst/scripts/syncAttachedReceiver.sh
+++ inst/scripts/syncAttachedReceiver.sh
@@ -26,9 +26,47 @@ WAITHI=${3/[^0-9]/}
 
 RUNNOW=1
 REMOTE=/sgm/remote
+RECEIVERDB=$REMOTE/receivers.sqlite
 SYNCFILE=$REMOTE/sync/$SERNO
 JOBFILE=$REMOTE/atjobs/$SERNO
-CONNECTION=$REMOTE/connections/${SERNO/SG-/}
+BARE_SERNO=${SERNO/SG-/}
+CONNECTION=$REMOTE/connections/$BARE_SERNO
+
+### <fix_issue_126>
+### temporary measure: fix tunnel port collisions
+### see: https://github.com/jbrzusto/motusServer/issues/126
+
+# array of bad ports by serial number of the SG we need to change it for
+
+declare -A BADPORT
+BADPORT[A4BCRPI27DE6]=40587
+BADPORT[392CRPI2EB35]=40588
+BADPORT[9E62RPI2EDEF]=40589
+BADPORT[4815BBBK1A47]=40590
+BADPORT[1315BBBK0136]=40591
+BADPORT[1914BBBK0929]=40592
+BADPORT[1315BBBK0083]=40594
+BADPORT[1215BBBK1031]=40595
+BADPORT[1315BBBK0112]=40596
+BADPORT[1215BBBK1749]=40597
+BADPORT[1215BBBK1778]=40598
+BADPORT[1315BBBK0110]=40599
+BADPORT[1215BBBK1796]=40600
+
+if [[ ${BADPORT[$BARE_SERNO} ]]; then
+    # see whether the tunnel port is the old, colliding one; i.e. <= 40600
+    PORT=`sqlite3 $RECEIVERDB "select tunnelport from receiver where serno='$BARE_SERNO'"`
+    if [[ ! "$PORT" ]]; then
+        # this is a receiver which needs a new tunnel port
+        # the easiest way to achieve this is to remove existing credentials
+        # for it on both the SG and server side, then cause it to reboot
+        sshpass -p root ssh -oStrictHostKeyChecking=no -p ${BADPORT[$BARE_SERNO]} root@localhost "cd /home/bone/.ssh; rm -f tunnel_port id_dsa id_dsa_pub; sleep 20; reboot"
+        /sgm/bin/remove_receiver.sh -y $BARE_SERNO
+        exit 4
+    fi
+fi
+
+### </fix_issue_126>
 
 # remove any existing at job for this receiver; we don't want sporadic
 # disconnect / reconnect by the receiver to launch multiple
