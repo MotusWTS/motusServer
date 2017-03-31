f6532e12240e2ee7505e3ae247877d16196d93c5
john brzustowski <jbrzusto@fastmail.fm>
2017-03-31 01:22:12 +0000

    correct location of remote receivers database in two scripts

--- inst/scripts/syncAttachedReceiver.sh
+++ inst/scripts/syncAttachedReceiver.sh
@@ -26,7 +26,7 @@ WAITHI=${3/[^0-9]/}
 
 RUNNOW=1
 REMOTE=/sgm/remote
-RECEIVERDB=/sgm/server.sqlite
+RECEIVERDB=/sgm/remote/receivers.sqlite
 SYNCFILE=$REMOTE/sync/$SERNO
 JOBFILE=$REMOTE/atjobs/$SERNO
 BARE_SERNO=${SERNO/SG-/}
