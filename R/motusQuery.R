MotusWTS/motusServer

commit 0e227e78bed9da66644486463f755493e56d26e2

retry queries that fail due to gnuTLS "unexpected length" issue; we desparately hope that the query eventually succeeds on retrying.
Closes #176 but not in a very satisfactory way...

R/motusQuery.R
@@ -76,22 +76,32 @@ motusQuery = function (API, params = NULL, requestType="post", show=FALSE, json=
 
     log = file("~/motus_query_log.txt", "a")
     cat(format(Sys.time()), ",", requestType, ",", API, ",", JSON, "\n", file=log)
-    tryCatch({
-        if (requestType == "post")
-            RESP = postForm(API, json=JSON, style="post", curl=curl)
-        else
-            RESP = getForm(API, json=JSON, curl=curl)
-        if (json)
-            return (RESP)
-        if (grepl("^[ \r\n]*$", RESP))
-            return(list())
-        rv = fromJSON(RESP)
-        cat(capture.output(RESP), "\n", file=log)
-        if (! is.null(rv$data))
-            return(rv$data)
-        return(rv)
-    }, error=function(e) {
-        cat("ERROR: ", as.character(e), "\n", file=log)
-        stop ("MotusQuery failed with error; ", as.character(e))
-    })
+    retries = 0
+    repeat {
+        tryCatch({
+            if (requestType == "post")
+                RESP = postForm(API, json=JSON, style="post", curl=curl)
+            else
+                RESP = getForm(API, json=JSON, curl=curl)
+            if (json)
+                return (RESP)
+            if (grepl("^[ \r\n]*$", RESP))
+                return(list())
+            rv = fromJSON(RESP)
+            if (retries > 0)
+                cat("Query was retried ", retries, " times due to gnuTLS issues\n", file=log)
+            cat(capture.output(RESP), "\n", file=log)
+            if (! is.null(rv$data))
+                return(rv$data)
+            return(rv)
+        }, error=function(e) {
+            if (any(grepl("TLS packet with unexpected length", as.character(e)))) {
+                Sys.sleep(5)
+                retries = retries + 1
+                next
+            }
+            cat("ERROR: ", as.character(e), "\n", file=log)
+            stop ("MotusQuery failed with error; ", as.character(e))
+        })
+    }
 }

commit 8e581a9a2991ee8494d75c5d067ce9c413e1ef08

fix bug introduced by commit 0e227e78bed9da66644486463f755493e56d26e2 sleepy programmer, apparently

R/motusQuery.R
@@ -77,7 +77,7 @@ motusQuery = function (API, params = NULL, requestType="post", show=FALSE, json=
     log = file("~/motus_query_log.txt", "a")
     cat(format(Sys.time()), ",", requestType, ",", API, ",", JSON, "\n", file=log)
     retries = 0
-    repeat {
+    while(retries < 5) {
         tryCatch({
             if (requestType == "post")
                 RESP = postForm(API, json=JSON, style="post", curl=curl)
@@ -97,11 +97,12 @@ motusQuery = function (API, params = NULL, requestType="post", show=FALSE, json=
         }, error=function(e) {
             if (any(grepl("TLS packet with unexpected length", as.character(e)))) {
                 Sys.sleep(5)
-                retries = retries + 1
-                next
             }
-            cat("ERROR: ", as.character(e), "\n", file=log)
-            stop ("MotusQuery failed with error; ", as.character(e))
+            if (retries > 5) {
+                cat("ERROR: ", as.character(e), "\n", file=log)
+                stop ("MotusQuery failed with error; ", as.character(e))
+            }
         })
+        retries <- retries + 1
     }
 }