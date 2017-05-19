d72654b4984bede75e9c67e408516c5412e6d25c
john brzustowski <jbrzusto@fastmail.fm>
2017-05-19 13:30:32 +0000

    fix quoting issues Closes #186

--- inst/scripts/motusCheckUser.sh
+++ inst/scripts/motusCheckUser.sh
@@ -8,13 +8,13 @@ DATE=`date -u +%Y%m%d%H%M%S`
 read LOGIN
 read PWORD
 printf -v JSON '{"date":"%s","login":"%s","pword":"%s", "type":"csv"}' "$DATE" "$LOGIN" "$PWORD"
-RESP=`curl -s --data-urlencode "json=$JSON" https://beta.motus.org/api/user/validate`
-RESP=${RESP//\'/\\\'}
-LOGIN=${LOGIN//\'/\\\'}
+RESP=`curl -s --data-urlencode "json=$JSON" https://motus.org/api/user/validate`
 if [[ "$RESP" =~ "userID" ]]; then
-    sqlite3 $USERDB "pragma busy_timeout=30000;create table if not exists userauth (login text unique primary key, userid integer, groups text);\
-                     replace into userauth (login, groups) values ('$LOGIN', '$RESP'); \
-                     update userauth set userid=json_extract(groups, '$.userID'), groups=json_extract(groups, '$.projects') where login='$LOGIN';"
+    LOGIN=${LOGIN//\'/\'\'};
+    RESP=${RESP//\'/\'\'};
+    echo "pragma busy_timeout=30000;create table if not exists userauth (login text unique primary key, userid integer, groups text);\
+          replace into userauth (login, groups) values ('$LOGIN', '$RESP');\
+          update userauth set userid=json_extract(groups, '$.userID'), groups=json_extract(groups, '$.projects') where login='$LOGIN';" | sqlite3 $USERDB > /dev/null
     exit 0;
 fi
 exit 1;
