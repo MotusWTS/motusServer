4ca448ef7303d89dcb265e8dd41609891a9abba3
John Brzustowski <admin@sensorgnome.org>
2017-06-26 19:56:32 +0000

    - fix various typos, and add "IF NOT EXISTS" to all "CREATE" statements

--- inst/motusTransferTableSchema.sql
+++ inst/motusTransferTableSchema.sql
@@ -1,10 +1,11 @@
 -- This is the schema for the tables used to transfer output from the tagfinder to motus.
 --
--- NOTE: individual SQL statements in this file are delimited by ';----', so that R code
--- can submit each statement to dbGetQuery() individually (dbGetQuery doesn't support
--- compound statements)
+-- NOTE: individual SQL statements in this file are delimited by a
+-- semicolon then two dashes, so that R code can submit each statement
+-- to dbGetQuery() individually (dbGetQuery doesn't support compound
+-- statements)
 
------------------------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------------------
 
 -- A batch is the result of processing one set of raw data files from
 -- a single receiver.  Batches are an artefact of how data is
@@ -20,7 +21,7 @@ CREATE TABLE IF NOT EXISTS batches (
                                                          -- foreign key to Motus DB table.
     monoBN INT,                                          -- boot number for this receiver (NULL
                                                          -- okay, e.g. Lotek)
-    tsBegin FLOAT(53) NOT NULL,                          -- timestamp for start of period
+    tsStart FLOAT(53) NOT NULL,                          -- timestamp for start of period
                                                          -- covered by batch
     tsEnd FLOAT(53) NOT NULL,                            -- timestamp for end of period
                                                          -- covered by batch
@@ -31,9 +32,9 @@ CREATE TABLE IF NOT EXISTS batches (
     motusProjectID INT,                                  -- user-selected motus project ID for this batch
     motusJobID INT                                       -- processing job which created this batch
 
-);----
+);--
 
-CREATE INDEX batches_tsMotus on batches(tsMotus);----
+CREATE INDEX IF NOT EXISTS batches_tsMotus on batches(tsMotus);--
 
 -- GPS fixes are recorded separately from tag detections.
 
@@ -45,7 +46,7 @@ CREATE TABLE IF NOT EXISTS gps (
     lon     FLOAT(53),                           -- longitude, decimal degrees
     alt     FLOAT(24),                           -- altitude, metres
     PRIMARY KEY (batchID, ts)
-);----
+);--
 
 
 -- A run is a sequence of detections of a single tag by a single
@@ -59,18 +60,18 @@ CREATE TABLE IF NOT EXISTS runs (
     runID BIGINT NOT NULL PRIMARY KEY,                -- identifier of run; unique for this receiver
     batchIDbegin INT NOT NULL,                        -- ID of batch this run begins in
     tsBegin FLOAT(53) NOT NULL,                       -- timestamp of first detection in the run
-    tsEnd FLOAT(53) NOT NULL                          -- timestamp of last detection in run so far (last pulse in completed burst, actually)
-    done TININT NOT NULL DEFAULT 0,                   -- is run finished? 0 if no, 1 if yes.
+    tsEnd FLOAT(53) NOT NULL,                         -- timestamp of last detection in run so far (last pulse in completed burst, actually)
+    done TINYINT NOT NULL DEFAULT 0,                  -- is run finished? 0 if no, 1 if yes.
     motusTagID INT NOT NULL,                          -- ID for the tag detected; foreign key to Motus DB
                                                       -- table; a negative value correspond to an entry in the tagAmbig table.
     ant TINYINT NOT NULL,                             -- antenna number (USB Hub port # for SG; antenna port
                                                       -- # for Lotek)
-    len BIGINT NOT NULL,                              -- number of detections in run ( so far ); this number
+    len BIGINT NOT NULL                               -- number of detections in run ( so far ); this number
                                                       -- can increase
-);----
+);--
 
-CREATE INDEX runs_motusTagID ON runs(motusTagID);----
-CREATE INDEX runs_batchIDbegin ON runs(batchIDbegin);----
+CREATE INDEX IF NOT EXISTS runs_motusTagID ON runs(motusTagID);--
+CREATE INDEX IF NOT EXISTS runs_batchIDbegin ON runs(batchIDbegin);--
 
 -- Because runs can span multiple batches, we want a way to
 -- keep track of which runs overlap which batches.
@@ -79,12 +80,12 @@ CREATE INDEX runs_batchIDbegin ON runs(batchIDbegin);----
 
 CREATE TABLE IF NOT EXISTS batchRuns (
     batchID INT NOT NULL REFERENCES batches,  -- batch ID
-    runID BIGINT NOT NULL REFERENCES runs     -- run ID
+    runID BIGINT NOT NULL REFERENCES runs,    -- run ID
     PRIMARY KEY (batchID, runID)              -- only one update per run per batch
-);----
+);--
 
-CREATE INDEX batchRuns_batchID ON batchRuns(batchID);----
-CREATE INDEX batchRuns_runID ON batchRuns(runID);----
+CREATE INDEX IF NOT EXISTS batchRuns_batchID ON batchRuns(batchID);--
+CREATE INDEX IF NOT EXISTS batchRuns_runID ON batchRuns(runID);--
 
 -- Hits are detections of tags.  They are grouped in two ways:
 -- by runs (consecutive detections of a single tag by a single antenna)
@@ -110,11 +111,11 @@ CREATE TABLE IF NOT EXISTS hits (
                                                       -- e.g. Lotek)
     burstSlop FLOAT (24)                              -- discrepancy of burst timing, in msec (NULL okay
                                                       -- e.g. Lotek)
-);----
+);--
 
-CREATE INDEX hits_batchID ON hits(batchID);----
-CREATE INDEX hits_runID ON hits(runID);----
-CREATE INDEX hits_ts on hits(ts);----
+CREATE INDEX IF NOT EXISTS hits_batchID ON hits(batchID);--
+CREATE INDEX IF NOT EXISTS hits_runID ON hits(runID);--
+CREATE INDEX IF NOT EXISTS hits_ts on hits(ts);--
 
 -- Table tagAmbig records sets of physically identical tags which have
 -- overlapping deployment periods.  When the motusTagID field in a row
@@ -136,10 +137,10 @@ CREATE TABLE IF NOT EXISTS tagAmbig (
     motusTagID5 INT,                       -- motus ID of tag in group.
     motusTagID6 INT,                       -- motus ID of tag in group.
     tsMotus FLOAT(53) NOT NULL DEFAULT -1  -- timestamp this record received by motus
-);----
+);--
 
-CREATE UNIQUE INDEX tagAmbig_motusTagID ON tagAmbig(motusTagID1, motusTagID2, motusTagID3, motusTagID4, motusTagID5, motusTagID6);----
-CREATE INDEX tagAmbig_tsMotus ON tagAmbig(tsMotus);----
+CREATE UNIQUE INDEX IF NOT EXISTS tagAmbig_motusTagID ON tagAmbig(motusTagID1, motusTagID2, motusTagID3, motusTagID4, motusTagID5, motusTagID6);--
+CREATE INDEX IF NOT EXISTS tagAmbig_tsMotus ON tagAmbig(tsMotus);--
 
 -- Table batchProgs records values of progVersion and progBuildTS by batchID.
 -- Note that receiver version of this table store these values incrementally (noting
@@ -155,9 +156,9 @@ CREATE TABLE IF NOT EXISTS batchProgs (
                                              -- seconds since 1 Jan 1970 GMT; NULL means not
                                              -- transferred
     PRIMARY KEY (batchID, progName)          -- only one version of a given program per batch
-);----
+);--
 
-CREATE INDEX batchProgs_batchID ON batchProgs(batchID);----
+CREATE INDEX IF NOT EXISTS batchProgs_batchID ON batchProgs(batchID);--
 
 CREATE TABLE IF NOT EXISTS batchParams (
 -- This table indicates what parameter values were used to run a btach.
@@ -171,9 +172,9 @@ CREATE TABLE IF NOT EXISTS batchParams (
     paramName VARCHAR(32) NOT NULL,            -- name of parameter (e.g. 'minFreq')
     paramVal FLOAT(53) NOT NULL,               -- value of parameter
     PRIMARY KEY (batchID, progName, paramName) -- only one value of a given parameter per program per batch
-);----
+);--
 
-CREATE INDEX batchParams_batchID ON batchParams(batchID);----
+CREATE INDEX IF NOT EXISTS batchParams_batchID ON batchParams(batchID);--
 
 CREATE TABLE IF NOT EXISTS pulseCounts (
 -- This table records antenna activity.  Neither Lotek nor SG
@@ -188,9 +189,9 @@ CREATE TABLE IF NOT EXISTS pulseCounts (
     hourBin INT,                             -- hour bin for this count; this is round(ts/3600)
     count   INT,                             -- number of pulses for given pcode in this file
     PRIMARY KEY (batchID, ant, hourBin)      -- a single count for each batchID, antenna, and hourBin
-);----
+);--
 
-CREATE INDEX pulseCounts_batchID ON pulseCounts(batchID);----
+CREATE INDEX IF NOT EXISTS pulseCounts_batchID ON pulseCounts(batchID);--
 
 CREATE TABLE IF NOT EXISTS tagDeployments (
        projectID INT NOT NULL,     -- motus project ID
@@ -199,7 +200,7 @@ CREATE TABLE IF NOT EXISTS tagDeployments (
        tsEnd FLOAT(53) NOT NULL,   -- unix timestamp of end of deployment
        INDEX tagDeployments_projectID (projectID),
        INDEX tagDeployments_motusTagID (motusTagID)
-);----
+);--
 
 CREATE TABLE IF NOT EXISTS receiverDeployments (
        projectID INT NOT NULL,     -- motus project ID
@@ -208,4 +209,14 @@ CREATE TABLE IF NOT EXISTS receiverDeployments (
        tsEnd FLOAT(53) NOT NULL,   -- unix timestamp of end of deployment
        INDEX receiverDeployments_projectID (projectID),
        INDEX receiverDeployments_deviceID (deviceID)
-);----
+);--
+
+CREATE TABLE IF NOT EXISTS maxKeys (
+-- This table records the maximum magnitude of the key value in one of the
+-- tables where clients reserve blocks of keys:
+-- batches, runs, hits, tagAmbig.
+-- This allows an appropriate query to atomically reserve a block of
+-- keys for a table, before writing data to it.
+       tableName CHAR(32) PRIMARY KEY,  -- table name in this DB
+       maxKey BIGINT                    -- maximum magnitude of "allocated" (reserved or actually used) key in this table
+);--
