5a64bbca05530ac935d65df52bd67422990d431a
John Brzustowski <admin@sensorgnome.org>
2017-11-09 18:06:10 +0000

    safe_arg: allow a mixed vector (list)

--- R/safe_arg.R
+++ R/safe_arg.R
@@ -11,11 +11,12 @@
 #'
 #' @param x list object with names
 #' @param name unquoted object name
-#' @param type unquoted type name, one of "character", "integer", "logical", "numeric"
+#' @param type unquoted type name, one of "character", "integer", "logical", "numeric", "list"
 #' or abbreviation thereof.
 #' @param scalar if TRUE (the default) return a vector of length 1,
 #' dropping any additional items. Otherwise, return a vector with the
-#' same length as the item from \code{x}.
+#' same length as the item from \code{x}.  If \code{type=="list"}, the
+#' vector is of mode "list".
 #'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
@@ -24,14 +25,16 @@ safe_arg = function(x, name, type, scalar=TRUE) {
     type = as.character(substitute(type))
     if (! name %in% names(x))
         return(NULL)
-    class = match.arg(type, c("character", "integer", "logical", "numeric"))
+    class = match.arg(type, c("character", "integer", "logical", "numeric", "list"))
 
     rv = as(x[[name]], class)
     if (isTRUE(scalar))
         rv = rv[1]
     if (length(rv) == 0 || isTRUE(all(is.na(rv))))
         return(NULL)
-    if (class == "character")
-        rv = gsub("'", "''", rv)
+    for (i in seq_len(length(rv))) {
+        if (class(rv[[i]]) == "character")
+            rv[[i]] = gsub("'", "''", rv[[i]])
+    }
     return(rv)
 }
