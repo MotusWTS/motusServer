01908bff1f0e7d1929e594e9668e0fc9a5ad813e
John Brzustowski <admin@sensorgnome.org>
2017-12-18 19:40:40 +0000

    clean formatting and typo re filesystem vs db file counts

--- inst/scripts/www/status2.js
+++ inst/scripts/www/status2.js
@@ -699,8 +699,8 @@ function show_recv_info3(x) {
                                  fmt_fileCount: function(i) {
                                      return this.day[i] + ':' +
                                          ((this.countFS[i] === this.countDB[i]) ?
-                                          ('     ' + this.countFS[i]).slice(-5)
-                                          : ('          ' + this.countFS[i] + '/' + this.countDB[i]).slice(-10))
+                                          ('         ' + this.countFS[i]).slice(-9)
+                                          : ('         ' + this.countFS[i] + '/' + this.countDB[i]).slice(-9))
                                          + '   ';
                                  },
                                  fileCounts: x.fileCounts
