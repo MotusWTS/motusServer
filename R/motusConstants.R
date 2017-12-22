2e564f19f5015fe496ccdaa89fe50ecc864f0e83
John Brzustowski <admin@sensorgnome.org>
2017-12-22 04:28:06 +0000

    temporarily revert to v 1.0 for search tags API
    ...until MotusDev/Motus_API#3 is closed

--- R/motusConstants.R
+++ R/motusConstants.R
@@ -6,7 +6,9 @@
 MOTUS_API_ENTRY_POINTS          = "http://motus.org/data/api/entrypoints.jsp"
 MOTUS_API_REGISTER_TAG          = "https://motus.org/api/tag/register"
 MOTUS_API_DEPLOY_TAG            = "https://motus.org/api/tag/deploy"
-MOTUS_API_SEARCH_TAGS           = "https://motus.org/api/tags/search"
+## 2017-12-21: revert to v1.0 for this until MotusDev/Motus_API#3 is closed
+##MOTUS_API_SEARCH_TAGS           = "https://motus.org/api/tags/search"
+MOTUS_API_SEARCH_TAGS           = "https://motus.org/data/api/v1.0/searchtags.jsp"
 MOTUS_API_USER_VALIDATE         = "https://motus.org/api/user/validate"
 MOTUS_API_REGISTER_RECEIVER     = "https://motus.org/api/receiver/register"
 MOTUS_API_LIST_PROJECTS         = "https://motus.org/api/projects"
