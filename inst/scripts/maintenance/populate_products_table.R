66b3a5565bd0bbfe62f99c055cffd520c0d0b7fd
John Brzustowski <admin@sensorgnome.org>
2018-02-28 14:44:24 +0000

    parse serial number from URL, rather than pulling from jobs table
    (upload jobs frequently include products for multiple receivers,
    so the previous scheme for assigning serno to product was faulty)
    
    Closes #375

--- inst/scripts/maintenance/populate_products_table.R
+++ inst/scripts/maintenance/populate_products_table.R
@@ -8,20 +8,22 @@ ensureServerDB()
 
 ## grab products from jobs where there's more than one
 
-allProds = ServerDB('select jobs.id as jobID, value as URL, ifnull(json_extract(data, "$.serno"),(select json_extract(data, "$.serno") as s2 from jobs as t2 where t2.pid is not null and t2.stump=jobs.id and s2 <> "" limit 1)) as serno from json_each(json_extract(jobs.data, "$.products_")), jobs where pid is null and json_array_length(jobs.data, "$.products_") > 0')
+allProds = ServerDB('select jobs.id as jobID, value as URL from json_each(json_extract(jobs.data, "$.products_")), jobs where json_array_length(jobs.data, "$.products_") > 0')
 
 ## jobs with single products store them as string scalars, not an array of length one, so we need a different
 ## use of the json_* functions:
 
-allProds = rbind(allProds, ServerDB('select jobs.id as jobID, json_extract(jobs.data, "$.products_") as URL, ifnull(json_extract(data, "$.serno"),(select json_extract(data, "$.serno") as s2 from jobs as t2 where t2.pid is not null and t2.stump=jobs.id and s2 <> "" limit 1)) as serno from jobs where pid is null and json_array_length(jobs.data, "$.products_") == 0 and json_extract(data, "$.products_") != ""'))
+allProds = rbind(allProds, ServerDB('select jobs.id as jobID, json_extract(jobs.data, "$.products_") as URL from jobs where json_array_length(jobs.data, "$.products_") == 0 and json_extract(data, "$.products_") != ""'))
 
 allProds = cbind(productID = 1:nrow(allProds), allProds)
 
-## extract project IDs from the URL; they will be first portion looking like `/NNN/`
-allProds = cbind(allProds, splitToDF("(?i)(?sx).*/(?<projectID>[0-9]+)/.*", allProds$URL))
+## extract project IDs and serial numbesr from the URLs, which look like "https://sgdata.motus.org/download/PROJECTID/SERNO-..."
+## we allow that some rows won't match, because they are for tag databases, not receiver-related files
+allProds = cbind(allProds, splitToDF("(?i)(?sx).*/(?<projectID>[0-9]+)/(?<serno>(Lotek-[0-9D]+)|(SG-[0-9A-Z]{12}))?.*", allProds$URL))
+allProds$serno[allProds$serno == ""] = NA
 
-## write to Server DB
-dbWriteTable(ServerDB$con, "products", allProds, row.names=FALSE, append=TRUE)
+## write to Server DB, ensuring columns are ordered correctly
+dbWriteTable(ServerDB$con, "products", allProds[,c("productID", "jobID", "URL", "serno", "projectID")], row.names=FALSE, append=TRUE)
 
 cat("Wrote ", nrow(allProds), " products to database.\n")
 
