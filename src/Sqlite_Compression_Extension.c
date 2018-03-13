4c21bb7754e190a2e669555429c0cc39f297901d
John Brzustowski <admin@sensorgnome.org>
2018-03-13 15:11:26 +0000

    reinstate gzcompress, gzuncompress in support of jbrzusto/find_tags#62

--- src/Sqlite_Compression_Extension.c
+++ src/Sqlite_Compression_Extension.c
@@ -11,6 +11,7 @@
 
   - add bzip2/bunzip2 support
   - add ability to decompress gzip files (strip header; use trailing 4-byte size)
+  - reinstate gzcompress, gzuncompress
 
 */
 
@@ -233,7 +234,47 @@ static void GZreadFile( sqlite3_context *context, int argc, sqlite3_value **argv
   fclose(in);
 }
 
+static void gzcompress( sqlite3_context *context, int argc, sqlite3_value **argv){
+  int nIn, nOut;
+  long int nOut2;
+  const unsigned char *inBuf;
+  unsigned char *outBuf;
+  assert( argc==1 );
+  nIn = sqlite3_value_bytes(argv[0]);
+  inBuf = sqlite3_value_blob(argv[0]);
+  nOut = 13 + nIn + (nIn+999)/1000;
+  outBuf = malloc( nOut+4 );
+  outBuf[0] = nIn>>24 & 0xff;
+  outBuf[1] = nIn>>16 & 0xff;
+  outBuf[2] = nIn>>8 & 0xff;
+  outBuf[3] = nIn & 0xff;
+  nOut2 = (long int)nOut;
+  compress(&outBuf[4], &nOut2, inBuf, nIn);
+  sqlite3_result_blob(context, outBuf, nOut2+4, free);
+}
 
+static void gzuncompress( sqlite3_context *context, int argc, sqlite3_value **argv){
+  unsigned int nIn, nOut, rc;
+  const unsigned char *inBuf;
+  unsigned char *outBuf;
+  long int nOut2;
+
+  assert( argc==1 );
+  nIn = sqlite3_value_bytes(argv[0]);
+  if( nIn<=4 ){
+    return;
+  }
+  inBuf = sqlite3_value_blob(argv[0]);
+  nOut = (inBuf[0]<<24) + (inBuf[1]<<16) + (inBuf[2]<<8) + inBuf[3];
+  outBuf = malloc( nOut );
+  nOut2 = (long int)nOut;
+  rc = uncompress(outBuf, &nOut2, &inBuf[4], nIn);
+  if( rc!=Z_OK ){
+    free(outBuf);
+  }else{
+    sqlite3_result_blob(context, outBuf, nOut2, free);
+  }
+}
 
 int sqlite3_extension_init(
                            sqlite3 *db,
@@ -244,7 +285,9 @@ int sqlite3_extension_init(
   sqlite3_create_function(db, "bz2compress", 1, SQLITE_UTF8, 0, &BZ2compressFunc, 0, 0);
   sqlite3_create_function(db, "bz2uncompress", 2, SQLITE_UTF8, 0, &BZ2uncompressFunc, 0, 0);
   sqlite3_create_function(db, "gzreadfile", -1, SQLITE_UTF8, 0, &GZreadFile, 0, 0);
-  sqlite3_create_function(db, "readfile", 1, SQLITE_UTF8, 0, readfileFunc, 0, 0);
-  sqlite3_create_function(db, "writefile", 3, SQLITE_UTF8, 0, writefileFunc, 0, 0);
+  sqlite3_create_function(db, "readfile", 1, SQLITE_UTF8, 0, &readfileFunc, 0, 0);
+  sqlite3_create_function(db, "writefile", 3, SQLITE_UTF8, 0, &writefileFunc, 0, 0);
+  sqlite3_create_function(db, "gzcompress", 1, SQLITE_UTF8, 0, &gzcompress, 0, 0);
+  sqlite3_create_function(db, "gzuncompress", 1, SQLITE_UTF8, 0, &gzuncompress, 0, 0);
   return 0;
 }
