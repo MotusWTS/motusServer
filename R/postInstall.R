a75eb2b9ecd38b3e2feedbf65264df6ac043bce2
John Brzustowski <admin@sensorgnome.org>
2018-07-23 11:50:40 +0000

    handle more regexes in postInstall to unify side effects - symlinks - shell commands
    This unifies:
    - installation of crontabs for backup, metadata refresh
    - web server config
    - script installation
    
    So really, I should be creating a debian package that installs
    the R package, instead of reinventing the inclined plane.

--- R/postInstall.R
+++ R/postInstall.R
@@ -1,61 +1,104 @@
 #' code to run after installing a new version of this package.
 #'
-#' After re-installing the motusServer package, this function is executed
-#' to fix ownership, permissions, and links outside the R package tree.
+#' @param root character scalar path to root of installation.
+#'     Default: "/" This parameter allows testing the function without
+#'     clobbering system files.  e.g. specifying
+#'     \code{root="/tmp/PItest"} will create the folder "/tmp/PItest"
+#'     and then install the root_overlay files there.
+#'
+#' @details After (re-)installing the motusServer package, this
+#'     function is executed to install files outside the R package
+#'     tree, and set ownership and permissions appropriately.  Details
+#'     of which files are installed is in the file
+#'     inst/root_overlay.md
 #'
 #' If you install the motusServer R package using this command line:
 #'
 #'    sudo su -c 'cd /home/sg/src/motusServer; /usr/local/bin/rpack -g .' sg
 #'
 #' then this function will be run by that script.  Otherwise, you can
-#' do so manually from the command line like so:
+#' do so manually from the shell like this:
 #'
 #'    Rscript -e 'motusServer:::postInstall()'
 #'
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-postInstall = function() {
-    S = function(...) system(sprintf(...))
+postInstall = function(root = "/") {
 
-    ## crontabs: jobs run periodically
+    ## parse out file config lines which look like:
+    ##  \code{%! shell command line} for a shell command to be run
+    ##  \code{% OWNER GROUP MODE PATH} for a file to be copied from the inst/root_overlay folder
+    ##  \code{% X -> Y} for a symlink to be created pointing from X to Y
 
-    crontabs = dir("/sgm_local/bin", pattern=".*_crontab", full.names=TRUE)
+    overlayRX = "(?mx)
+# lines giving files start with a percent sign
+^%
 
-    ## change ownership and permissions because cron requires root
-    ## ownership and that the files not be writable by group or others
-    ## and then make sure there are links to the crontabs in
-    ## /etc/cron.d
+((
+# shell command
+!
+(?<cmd>.*$)
+)
 
-    for (t in crontabs) {
-        S("sudo chown root:root %s", t)
-        S("sudo chmod og-w %s", t)
-        S("sudo ln -f -s %s /etc/cron.d", t)
+|
+
+(
+# symbolic file owner
+[[:space:]]+
+(?<owner>[^[:space:]]++)
+
+[[:space:]]++
+
+# symbolic file group
+(?<group>[^[:space:]]++)
+
+[[:space:]]++
+
+# file permissions
+(?<mode>[^[:space:]]++)
+
+[[:space:]]++
+
+# absolute file path, also path relative to root_overlay folder
+
+(?<path>[^[:space:]]++)
+)
+
+|
+
+(
+# optional symlink src and destination
+[[:space:]]+
+
+(?<sym_src>[^[:space:]]++)
+
+(?:[[:space:]]*)
+
+->
+
+(?:[[:space:]]*)
+
+(?<sym_dst>[^[:space:]]*)
+
+)
+)
+"
+    src_root = system.file("root_overlay", package="motusServer")
+    ov = splitToDF(overlayRX, readLines(system.file("root_overlay.md", package="motusServer")), guess=FALSE)
+
+    SUDO = function(a, ...) {
+        system(sprintf(paste("sudo", a), ...))
     }
-
-    ensureServerDB(installing=TRUE)
-    openMotusDB()
-    ensureMotusTransferTables()
-
-    ## create links in the web server root direcotry
-
-    ## download directory
-    S("sudo ln -s /sgm/www /var/www/html/download")
-    S("sudo ln -s /sgm/www/index.html/index.html /var/www/html/index.html")
-    S("sudo ln -s /sgm/www/index.php/index.php /var/www/html/index.php")
-
-    ## ugly php login page
-    S("sudo ln -s /sgm/www/login.php/login.php /var/www/html/login.php")
-
-    ## public pages not requiring login
-    S("sudo ln -s /sgm/pub /var/www/html/public")
-
-    ## additional message for status page
-    S("sudo ln -s /sgm_local/bin/www/status_message.html /var/www/html/status_message.html")
-
-    ## upload pages (yes, directly from the source folder.  Not smart!)
-    S("sudo ln -s /home/sg/src/ProjectSend /var/www/html/upload")
-
-    ## robots.txt - keep webcrawlers off this server; at most, the /sgm/pub folder would
-    ## be worth indexing, but there should be other links to all that content/
-    S("sudo ln -s /sgm_local/bin/www/robots.txt /var/www/html/robots.txt")
+    for (i in seq_len(nrow(ov))) {
+        if (ov$cmd[i] != "") {
+            system(ov$cmd[i])
+        } else if (ov$sym_src[i] == "") {
+            ## copy normal file
+            src = file.path(src_root, ov$path[i])
+            SUDO("cp '%s'  '%s';       sudo  chown %s:%s '%s';               sudo chmod %s '%s'",
+                                     src, ov$path[i], ov$owner[i], ov$group[i], ov$path[i], ov$mode[i], ov$path[i])
+        } else {
+            SUDO("ln -s -f '%s' '%s'", ov$sym_dst[i], ov$sym_src[i])
+        }
+    }
 }
---
+++ inst/root_overlay.md
@@ -0,0 +1,68 @@
+### motusServer Root Filesystem Overlay ###
+
+The `motusServer` package provides some files which must be installed outside
+the usual R package tree.  This file documents those files, and provides
+the required ownership and permission information in a form accepted by
+`motusServer:::postInstall()`.  Any line in this file which begins with
+a percent sign (**%**) is parsed by that function as white-space separated
+items `USER GROUP PERMISSIONS PATH` where:
+
+ - `USER` and `GROUP` are the desired symbolic file owner and group
+ - `PERMISSIONS` is the desired file permission string, in octal format
+ - `PATH` is the path to the file relative to root
+
+The files to be installed are in the `inst/root` tree of the package repo, with
+generic ownership and permissions.
+
+For example, the line `% root root 644 /etc/ssh/sshd_sg_config` indicates the
+file `inst/root_overlay/etc/ssh/sshd_sg_config` will be copied to `/etc/ssh/sshd_sg_config`,
+its ownership will be `root:root`, and its mode will be 644 (rw-r-r-).
+
+Here are the files:
+
+```
+% root root 644 /etc/apache2/sites-available/001-sgdata-ssl.conf
+% root root 644 /etc/apache2/sites-available/002-sgdata-nossl.conf
+% root root 644 /etc/cron.d/refreshMotusMetaDB
+% root root 644 /etc/cron.d/sqlite_daily_backup
+% root root 755 /etc/init.d/sshd-sg
+
+```
+And here are symlinks we need to connect some external programs to content
+provided by this package.  These are given as `% src -> dst`
+
+```
+# download directory
+% /var/www/html/download -> /sgm/www
+% /var/www/html/index.html -> /sgm/www/index.html
+% /var/www/html/index.php -> /sgm/www/index.php
+
+# ugly php login page
+% /var/www/html/login.php -> /sgm/www/login.php
+
+# public pages not requiring login
+% /var/www/html/public -> /sgm/pub
+
+# additional message for status page
+% /var/www/html/status_message.html -> /sgm_local/bin/www/status_message.html
+
+# upload pages (yes, directly from the source folder.  Not smart!)
+% /var/www/html/upload -> /home/sg/src/ProjectSend
+
+# robots.txt - keep webcrawlers off this server; at most, the /sgm/pub folder would
+# be worth indexing, but there should be other links to all that content elsewhere.
+% /var/www/html/robots.txt -> /sgm_local/bin/www/robots.txt
+
+```
+
+Commands to enable use of some files etc.
+
+```
+# enable the two websites (nossl simply redirects to ssl)
+%! sudo a2ensite 001-sgdata-ssl
+%! sudo a2ensite 002-sgdata-nossl
+
+# restart the web server gracefully
+%! sudo apachectl graceful
+
+```
