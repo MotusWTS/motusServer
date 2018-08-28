9d62e3b952fd2b1ebf46b28451a61321abcb9efe
John Brzustowski <admin@sensorgnome.org>
2018-08-28 15:11:20 +0000

    accept _N suffix to disambiguate Lotek receivers with dup sernos

--- R/motusConstants.R
+++ R/motusConstants.R
@@ -181,7 +181,7 @@ MOTUS_ARCHIVE_DIR_REGEX = paste0("\\.(", paste(MOTUS_ARCHIVE_SUFFIXES, collapse=
 MOTUS_SG_SERNO_REGEX = "(?i)(?<serno>SG-[0-9A-Z]{4}(?:RPi[123z]|BBBK|(BB[0-9][0-9A-Z]))[0-9A-Z]{4}(?:_[0-9])?)"
 
 ## regex to exactly match any receiver serial number
-MOTUS_RECV_SERNO_REGEX = "(?i)^(?:(?:(?:SG-[0-9A-Z]{4}(?:RPi[123z]|BBBK|(?:BB[0-9][0-9A-Z]))[0-9A-Z]{4}(?:_[0-9])?))|(?:Lotek-D?[0-9]+))$"
+MOTUS_RECV_SERNO_REGEX = "(?i)^(?:(?:(?:SG-[0-9A-Z]{4}(?:RPi[123z]|BBBK|(?:BB[0-9][0-9A-Z]))[0-9A-Z]{4}(?:_[0-9])?))|(?:Lotek-D?[0-9]+(?:_[0-9])?))$"
 
 ## regex for matching DOS filenames (names of SG data files which have
 ## been shortened to 8.3 form)
