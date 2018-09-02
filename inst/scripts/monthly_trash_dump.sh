60ece5d30c83c2ed8964f79fc3682189678cf1a7
John Brzustowski <admin@sensorgnome.org>
2018-09-02 00:05:31 +0000

    add ';' missing from find command's -exec option

--- inst/scripts/monthly_trash_dump.sh
+++ inst/scripts/monthly_trash_dump.sh
@@ -7,4 +7,4 @@ DATE=`date -Is`
 LOGFILE=/sgm/logs/trash.txt
 
 echo "$DATE: Deleting folders older than 31 days from $TRASHDIR" >> $LOGFILE
-find $TRASHDIR -maxdepth 1 -type d -mtime +31 -exec rm -rf '{}'
+find $TRASHDIR -maxdepth 1 -type d -mtime +31 -exec rm -rf '{}' ';'
