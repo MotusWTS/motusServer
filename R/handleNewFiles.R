d8287d68463e7afdfd213bfc4d83f943e7dadbd6
JeremyHussell <JeremyHussell@yahoo.ca>
2019-10-17 10:00:42 -0400

    Remove extra parenthesis

--- R/handleNewFiles.R
+++ R/handleNewFiles.R
@@ -60,7 +60,7 @@ handleNewFiles = function(j) {
     ## move CTT SensorGnome and SensorStation data files
     cttdata = grep(paste0("(?i)(?:", MOTUS_CTT_SG_DATAFILE_REGEX, ")|(?:", MOTUS_CTT_SS_DATAFILE_REGEX, ")"), all, perl=TRUE)
     if (length(cttdata)) {
-        moveFilesUniquely(all[cttdata], MOTUS_PATH$CTT_OUTGOING))
+        moveFilesUniquely(all[cttdata], MOTUS_PATH$CTT_OUTGOING)
         eg = all[cttdata[1]]
         all = all[ - cttdata]
         jobLog(j, paste0("Moved ", length(cttdata), " CTT files for separate processing with names like\n   ", eg))
