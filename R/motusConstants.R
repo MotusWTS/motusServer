61e043e1da9404335dd7b8b5f4f13fbc35842703
Denis Lepage <dlepage@bsc-eoc.org>
2021-01-22 02:50:09 -0500

    Sigma Eight Regex

--- R/motusConstants.R
+++ R/motusConstants.R
@@ -181,8 +181,10 @@ MOTUS_LOTEK_SERNO_REGEX = "Lotek-D?[0-9]+(?:_[0-9])?"
 
 MOTUS_CTT_SERNO_REGEX = "CTT-(?:[0-9]{15}|[0-9A-F]{12})"
 
+MOTUS_SEI_SERNO_REGEX = "SEI_[AO]_[0-9]{2}(0[0-9]|1[0-2])([0-2][0-9]|3[0-1])[0-9]{3}"
+
 ## regex to exactly match any receiver serial number
-MOTUS_RECV_SERNO_REGEX = paste0("(?i)^(?:(?:", MOTUS_SG_SERNO_REGEX, ")|(?:", MOTUS_LOTEK_SERNO_REGEX, ")|(?:", MOTUS_CTT_SERNO_REGEX, "))$")
+MOTUS_RECV_SERNO_REGEX = paste0("(?i)^(?:(?:", MOTUS_SG_SERNO_REGEX, ")|(?:", MOTUS_LOTEK_SERNO_REGEX, ")|(?:", MOTUS_CTT_SERNO_REGEX, ")|(?:", MOTUS_SEI_SERNO_REGEX, "))$")
 
 ## regex for matching DOS filenames (names of SG data files which have
 ## been shortened to 8.3 form)
