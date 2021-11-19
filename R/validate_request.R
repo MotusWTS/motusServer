8bef61ed58c350d5a2c8c46af34ddd74632eeb95
JeremyHussell <JeremyHussell@yahoo.ca>
2021-11-19 10:45:06 -0500

    Collect diagnostics for issue #1019

--- R/validate_request.R
+++ R/validate_request.R
@@ -146,27 +146,29 @@ token = authToken)
         env = parent.frame()$env
         forwardedFor = env$HTTP_X_FORWARDED_FOR
         if(!is.character(forwardedFor)) {
-            forwardedFor = env$HTTP_ED_FOR ## Header name is getting truncated somehow, for some specific people.
-        }
-        remoteIP = strsplit(forwardedFor, ", ", fixed=TRUE)[[1]][1]
-        iptstamp = c(as.raw(as.integer(strsplit(remoteIP, ".", fixed=TRUE)[[1]])), rev(packBits(intToBits(hextimestamp))))
-        user_id = substring(parts[1], 41)
-
-        ## This bit mimics what happens in login.php.  In the first case, we're using a byte sequence.
-
-        digest0 = digest::digest(c(iptstamp, MOTUS_SECRETS$mod_auth_tkt, charToRaw(user_id), as.raw(0), charToRaw(token_list), as.raw(0), charToRaw(user_data)), algo="md5", serialize=FALSE, ascii=TRUE)
-
-        ## But the second time, we use the ascii representation of the
-        ## digest from above, rather than its raw bytes.  Weird, but
-        ## matches what happens in apache's mod-auth-tkt so that's
-        ## what we have to do.
-
-        digest = digest::digest(paste0(digest0, rawToChar(MOTUS_SECRETS$mod_auth_tkt)), serialize=FALSE, algo="md5")
-
-        if (ticket_digest != digest) {
-            msg = "invalid authorization ticket"
+            msg = "X_FORWARDED_FOR HTTP header missing. This bug is difficult to reproduce, so a bunch of diagnostics just got emailed to the administrator. If you're reading this: thank you for triggering the bug, we might be able to figure out what's going on and fix it soon thanks to the diagnostics. Sorry for interruption of service."
+            email(MOTUS_ADMIN_EMAIL, "Issue #1019 diagnostics", paste0(ls(env), collapse="\n"))
         } else {
-            auth = list(userID=as.integer(user_id), projects=token_list, userType=user_data)
+            remoteIP = strsplit(forwardedFor, ", ", fixed=TRUE)[[1]][1]
+            iptstamp = c(as.raw(as.integer(strsplit(remoteIP, ".", fixed=TRUE)[[1]])), rev(packBits(intToBits(hextimestamp))))
+            user_id = substring(parts[1], 41)
+
+            ## This bit mimics what happens in login.php.  In the first case, we're using a byte sequence.
+
+            digest0 = digest::digest(c(iptstamp, MOTUS_SECRETS$mod_auth_tkt, charToRaw(user_id), as.raw(0), charToRaw(token_list), as.raw(0), charToRaw(user_data)), algo="md5", serialize=FALSE, ascii=TRUE)
+
+            ## But the second time, we use the ascii representation of the
+            ## digest from above, rather than its raw bytes.  Weird, but
+            ## matches what happens in apache's mod-auth-tkt so that's
+            ## what we have to do.
+
+            digest = digest::digest(paste0(digest0, rawToChar(MOTUS_SECRETS$mod_auth_tkt)), serialize=FALSE, algo="md5")
+
+            if (ticket_digest != digest) {
+                msg = "invalid authorization ticket"
+            } else {
+                auth = list(userID=as.integer(user_id), projects=token_list, userType=user_data)
+            }
         }
     }
     isAdmin = isTRUE(auth$userType == "administrator")
