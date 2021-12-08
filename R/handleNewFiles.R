ae9d107df3f360b0ae08c905baf630cae9f0b341
JeremyHussell <JeremyHussell@yahoo.ca>
2021-12-08 13:52:50 -0500

    Test fix for symlinks in ctt_outgoing

--- R/handleNewFiles.R
+++ R/handleNewFiles.R
@@ -60,7 +60,8 @@ handleNewFiles = function(j) {
     ## move CTT SensorGnome and SensorStation data files
     cttdata = grep(paste0("(?i)(?:", MOTUS_CTT_SG_DATAFILE_REGEX, ")|(?:", MOTUS_CTT_SS_DATAFILE_REGEX, ")"), all, perl=TRUE)
     if (length(cttdata)) {
-        moveFilesUniquely(all[cttdata], MOTUS_PATH$CTT_OUTGOING, copyLinkTargets=TRUE)
+        success = file.copy(all[cttdata], file.path(MOTUS_PATH$CTT_OUTGOING, basename(all[cttdata])))
+        file.remove(all[cttdata][success])
         eg = all[cttdata[1]]
         all = all[ - cttdata]
         jobLog(j, paste0("Moved ", length(cttdata), " CTT files for separate processing with names like\n   ", eg))
